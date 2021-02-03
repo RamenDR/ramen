9ef12baa20fcc03cb2f4a3b870b511eec33a382f
Veera Deenadhayalan <vdeenadh@redhat.com>
2021-02-02 23:54:46 -0800

    Initial reconciler function

--- controllers/volumereplicationgroup_controller.go
+++ controllers/volumereplicationgroup_controller.go
@@ -20,6 +20,8 @@ import (
 	"context"
 
 	"github.com/go-logr/logr"
+	"github.com/prometheus/common/log"
+	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -50,9 +52,84 @@ type VolumeReplicationGroupReconciler struct {
 func (v *VolumeReplicationGroupReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = v.Log.WithValues("volumereplicationgroup", req.NamespacedName)
 
-	// your logic here
+	// Fetch the VolumeReplicationGroup instance
+	volRepGroup := &ramendrv1alpha1.VolumeReplicationGroup{}
+	err := v.Get(ctx, req.NamespacedName, volRepGroup)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("VolumeReplicationGroup resource not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get VolumeReplicationGroup")
+		return ctrl.Result{}, err
+	}
+	log.Info("Processing VolumeReplicationGroup ", volRepGroup.Spec, " in ns: ", req.NamespacedName)
+
+	/*
+		// Check if the VolumeReplication CR already exists for the PVs of this application, if not create a new one
+		for all PVs of this application {
+			volRep := &appsv1.Deployment{}
+			err = v.Get(ctx, types.NamespacedName{Name: volRepGroup.Name, Namespace: volRepGroup.Namespace}, volRep)
+			if err != nil && errors.IsNotFound(err) {
+				// Define a new VolumeReplication CR
+				newVolRep := v.deploymentForMemcached(volRepGroup)
+				log.Info("Creating a new VolumeReplication CR", "Deployment.Namespace", newVolRep.Namespace, "Deployment.Name", newVolRep.Name)
+				err = v.Create(ctx, newVolRep)
+				if err != nil {
+					log.Error(err, "Failed to create new VolumeReplication CR", "Deployment.Namespace", newVolRep.Namespace, "Deployment.Name", newVolRep.Name)
+					return ctrl.Result{}, err
+				}
+				// VolumeReplication CR created successfully - return and requeue
+				return ctrl.Result{Requeue: true}, nil
+			} else if err != nil {
+				log.Error(err, "Failed to get VolumeReplication CR")
+				return ctrl.Result{}, err
+			}
+
+			// Ensure the VolumeReplication fields as the same as the VolumeReplicationGroup spec
+			size := volRepGroup.Spec.Size
+			if *volRep.Spec.Replicas != size {
+				volRep.Spec.Replicas = &size
+				err = v.Update(ctx, volRep)
+				if err != nil {
+					log.Error(err, "Failed to update Deployment", "Deployment.Namespace", volRep.Namespace, "Deployment.Name", volRep.Name)
+					return ctrl.Result{}, err
+				}
+				// Spec updated - return and requeue
+				return ctrl.Result{Requeue: true}, nil
+			}
+		}
+
+		// Update the VolumeReplicationGroup status using the children VolumeReplication CRs
+		// List the VolumeReplication CRs for this volRepGroup
+		volRepList := &corev1.VolumeReplicationList{}
+		listOpts := []client.ListOption{
+			client.InNamespace(volRepGroup.Namespace),
+			client.MatchingLabels(labelsForVolumeReplication(volRepGroup.Name)),
+		}
+		if err = v.List(ctx, volRepList, listOpts...); err != nil {
+			log.Error(err, "Failed to list VolumeReplication CRs", "VolumeReplicationGroup.Namespace", volRepGroup.Namespace, "VolumeReplicationGroup.Name", volRepGroup.Name)
+			return ctrl.Result{}, err
+		}
+		volRepNames := getVolRepNames(volRepList.Items)
+
+		// Update status.Nodes if needed
+		if !reflect.DeepEqual(volRepNames, volRepGroup.Status.VolumeReplications) {
+			volRepGroup.Status.VolumeReplications = volRepNames
+			err := v.Status().Update(ctx, volRepGroup)
+			if err != nil {
+				log.Error(err, "Failed to update VolumeReplicationGroup status")
+				return ctrl.Result{}, err
+			}
+		}
+	*/
 
 	return ctrl.Result{}, nil
+
 }
 
 // SetupWithManager sets up the controller with the Manager.
