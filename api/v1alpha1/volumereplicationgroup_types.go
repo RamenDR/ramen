40f5ef8aa611610a1d95bbb45a68dbe741d466a9
Veera Deenadhayalan <vdeenadh@redhat.com>
2021-03-17 22:40:45 -0700

    Rename VRG Spec fields and improve type comments
    - PVCLabelSelector is now PVCSelector
    - VolumeState is now ReplicationState to match with VR spec
    - Comment out match expressions in VRG sample yaml file
    - Use volrep shim's ReplicationState definition in VRG type

--- api/v1alpha1/volumereplicationgroup_types.go
+++ api/v1alpha1/volumereplicationgroup_types.go
@@ -19,37 +19,37 @@ package v1alpha1
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
-	vrv1alpha1 "github.com/kube-storage/volume-replication-operator/api/v1alpha1"
+	volrep "github.com/shyamsundarr/volrep-shim-operator/api/v1alpha1"
 )
 
 // NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 // VolumeReplicationGroup (VRG) spec declares the desired replication class
-// and replication state of all the PVCs owned by a given application, which
-// are identified via the given PVC label selector.  For every PVC created
-// by the application, the VRG operator does the following:
+// and replication state of all the PVCs identified via the given PVC label
+// selector.  For each such PVC, the VRG will do the following:
 // 	- Create a VolumeReplication (VR) CR to enable storage level replication
-// 	  of volume data and set the volume state to primary, secondary, etc.
+// 	  of volume data and set the desired replication state (primary, secondary,
+//    etc).
 //  - Take the corresponding PV metadata in Kubernetes etcd and deposit it in
 //    the S3 store.  The url, access key and access id required to access the
 //    S3 store is specified via environment variables of the VRG operator POD,
 //    which is obtained from a secret resource.
-//  - Manage the lifecycle of VR CR and S3 data to match the CUD operations on
-//    VRG CR.
+//  - Manage the lifecycle of VR CR and S3 data according to CUD operations on
+//    the PVC and the VRG CR.
 type VolumeReplicationGroupSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	// Label selector to identify all the PVCs that are in this group
-	// This operator will create and needs to be replicated
-	PVCLabelSelector metav1.LabelSelector `json:"pvcLabelSelector"`
+	// that needs to be replicated to the peer cluster.
+	PVCSelector metav1.LabelSelector `json:"pvcSelector"`
 
-	// ReplicationClass of all volumes in this replication group that is
-	// passed to VolumeReplication CR
+	// ReplicationClass of all volumes in this replication group;
+	// this value is propagated to children VolumeReplication CRs
 	VolumeReplicationClass string `json:"volumeReplicationClass"`
 
-	// Desired state of all volumes [primary or secondary] in this replication group that is
-	// passed to VolumeReplication CR
-	VolumeState vrv1alpha1.ImageState `json:"volumeState"`
+	// Desired state of all volumes [primary or secondary] in this replication group;
+	// this value is propagated to children VolumeReplication CRs
+	ReplicationState volrep.ReplicationState `json:"replicationState"`
 }
 
 // VolumeReplicationGroupStatus defines the observed state of VolumeReplicationGroup
