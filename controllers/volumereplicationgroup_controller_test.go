733ed24268fd2de344b7dc0c5c9a442f82e82c3b
Benamar Mekhissi <bmekhiss@redhat.com>
2021-08-16 06:11:33 -0400

    fix relocate not waiting for VRG to becom secondary in all clusters

--- controllers/volumereplicationgroup_controller_test.go
+++ controllers/volumereplicationgroup_controller_test.go
@@ -580,7 +580,7 @@ func (v *vrgTest) createVRG(pvcLabels map[string]string) {
 			ReplicationState:         "primary",
 			SchedulingInterval:       schedulingInterval,
 			ReplicationClassSelector: metav1.LabelSelector{MatchLabels: replicationClassLabels},
-			S3ProfileList:            []string{"fakeS3Profile"},
+			// S3ProfileList:            []string{"fakeS3Profile"},
 		},
 	}
 	err := k8sClient.Create(context.TODO(), vrg)
