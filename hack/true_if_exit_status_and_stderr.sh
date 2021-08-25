50aba870e3b0b830d4e7edee0c8a450f0c693329
hatfieldbrian <bhatfiel@redhat.com>
2021-08-25 11:48:19 -0700

    ocm-minikube-ramen.sh: add failover and relocate commands
    	- rename ramen and rook_ceph deploy/undeploy routines
    		- remove suffix '_all' from multi-cluster routines
    		- add suffix '_hub_or_spoke' or '_spoke' to single-cluster routines
    	- deploy and undeploy busybox-sample namespace
    	- deploy a minio to each spoke, not hub, and wait up to 1 minute for availability
    	- define cephcsi and volume-replication image deployment routines
    	- define routine to ignore command failure for specific exit status and standard error
    
    Signed-off-by: hatfieldbrian <bhatfiel@redhat.com>

---
+++ hack/true_if_exit_status_and_stderr.sh
@@ -0,0 +1,26 @@
+# shellcheck shell=sh
+true_if_exit_status_and_stderr()
+{
+	exit_status_expected=$1
+	stderr_expected=$2
+	shift 2
+	stderr_pipe_name1=/tmp/$$.err1 stderr_pipe_name2=/tmp/$$.err2
+	rm -f $stderr_pipe_name1 $stderr_pipe_name2
+	mkfifo -m o=rw $stderr_pipe_name1 $stderr_pipe_name2
+	tee $stderr_pipe_name2 <$stderr_pipe_name1 >&2 &
+	tee_pid=$!
+	case $- in *e*) e=-e; set +e;; *) e= ; esac
+	"$@" 2>$stderr_pipe_name1
+	set $e -- $?
+	unset -v e
+	stderr=$(cat $stderr_pipe_name2)
+	wait $tee_pid
+	unset -v tee_pid
+	rm -f $stderr_pipe_name1 $stderr_pipe_name2
+	unset -v stderr_pipe_name1 stderr_pipe_name2
+	if test "$1" -eq "$exit_status_expected" && test "$stderr" = "$stderr_expected"; then
+		set -- 0
+	fi
+	unset -v stderr stderr_expected exit_status_expected
+	return "$1"
+}
