a12e92a44886689d7b726126f09f2f913415b599
hatfieldbrian <bhatfiel@redhat.com>
2021-12-20 02:21:32 -0800

    ocm-minikube.sh: specify subscription operator image tag matching manifests
    	- replace subscription operator repository cloning with manifest file urls
    	- fix helmrepo channel example by specifying cluster to deploy to
    	- define routines to deploy and undeploy subscription operator on all clusters
    	- define routine to create hub kubeconfig file
    	- define github-url.sh containing routines to construct file and directory urls
    
    Signed-off-by: hatfieldbrian <bhatfiel@redhat.com>

---
+++ hack/github-url.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+github_url_file()
+{
+        echo https://raw.githubusercontent.com/"$1"/"$3"/"$2"
+}
+github_url_directory()
+{
+        echo https://github.com/"$1"/"$2"?ref="$3"
+}
+github_url_unset()
+{
+	unset -f github_url_file
+	unset -f github_url_directory
+	unset -f github_url_unset
+}
