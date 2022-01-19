90ea18f1bec5eba28a2091f8ff73f91737c1c4e4
Raghavendra Talur <raghavendra.talur@gmail.com>
2022-01-19 10:32:01 -0500

    hack: bump minikube version to v1.24.0
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- hack/minikube-install.sh
+++ hack/minikube-install.sh
@@ -4,7 +4,7 @@ if ! command -v minikube; then
 	# https://minikube.sigs.k8s.io/docs/start/
 	$(dirname ${0})/curl-install.sh ${1}
 	minikube_version=latest
-	minikube_version=v1.18.1
+	minikube_version=v1.24.0
 	curl -LRo ${1}/minikube https://storage.googleapis.com/minikube/releases/${minikube_version}/minikube-linux-amd64
 	unset -v minikube_version
 	chmod +x ${1}/minikube
