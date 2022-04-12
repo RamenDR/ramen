0d6ee257fc69ce56ef2cab7f5a991ae62feb5864
Benamar Mekhissi <bmekhiss@redhat.com>
2022-04-12 18:25:02 -0400

    Remove all subscription code

--- controllers/drplacementcontrol.go
+++ controllers/drplacementcontrol.go
@@ -26,7 +26,6 @@ import (
 	ocmworkv1 "github.com/open-cluster-management/api/work/v1"
 	errorswrapper "github.com/pkg/errors"
 	plrv1 "github.com/stolostron/multicloud-operators-placementrule/pkg/apis/apps/v1"
-	subv1 "github.com/stolostron/multicloud-operators-subscription/pkg/apis/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -549,8 +548,6 @@ func (d *DRPCInstance) RunRelocate() (bool, error) {
 	}
 
 	if curHomeCluster != preferredCluster {
-		sub := &subv1.Subscription{}
-		d.log.Info("sub", "sub", sub)
 		if d.hasVRGTakenPVCsOwnership(curHomeCluster) {
 			err := d.sendRequestToTakePVCsOwnership(curHomeCluster)
 			if err != nil {
