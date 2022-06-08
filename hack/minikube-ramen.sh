056e967b03b2e20aee0be4f62ae8155be000bb7b
hatfieldbrian <bhatfiel@redhat.com>
2022-06-08 14:45:15 -0700

    Script sample application deployment and protection without ocm
    	- Define the following routines in hack/minikube-ramen.sh:
    		- application_sample_deploy
    		- application_sample_undeploy
    		- application_sample_vrg_deploy
    		- application_sample_vrg_undeploy
    	- Document sample application support in hack/README.md
    
    Signed-off-by: hatfieldbrian <bhatfiel@redhat.com>

--- hack/minikube-ramen.sh
+++ hack/minikube-ramen.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 # shellcheck disable=2086
-set -x
-set -e
 ramen_hack_directory_path_name=$(dirname $0)
 cluster_names=${cluster_names:-cluster1\ cluster2}
 deploy()
@@ -33,10 +31,74 @@ manager_redeploy()
 		minikube -p $cluster_name ssh -- docker images\|grep ramen
 	done; unset -v cluster_name
 }
+application_sample_namespace_name=${application_sample_namespace_name:-default}
+application_sample_namespace_deploy()
+{
+	kubectl create namespace $application_sample_namespace_name --dry-run=client -oyaml|kubectl --context $1 apply -f-
+}
+application_sample_namespace_undeploy()
+{
+	kubectl --context $1 delete namespace $application_sample_namespace_name
+}
+application_sample_kubectl()
+{
+	kubectl --context $1 -n$application_sample_namespace_name $2 -khttps://github.com/RamenDR/ocm-ramen-samples/busybox
+}
+application_sample_deploy()
+{
+	application_sample_kubectl $1 apply
+}
+application_sample_undeploy()
+{
+	application_sample_kubectl $1 delete
+}
+application_sample_vrg_kubectl()
+{
+	cat <<-a|kubectl --context $1 -n$application_sample_namespace_name $3 -f-
+	---
+	apiVersion: ramendr.openshift.io/v1alpha1
+	kind: VolumeReplicationGroup
+	metadata:
+	  name: bb
+	spec:
+	  async:
+	    mode: Enabled
+	    replicationClassSelector: {}
+	    schedulingInterval: 1m
+	  pvcSelector:
+	    matchLabels:
+	      appname: busybox
+	  replicationState: primary
+	  s3Profiles:
+	  - minio-on-$1
+	  - minio-on-$2
+	  sync:
+	    mode: Disabled
+	a
+}
+application_sample_vrg_deploy()
+{
+	application_sample_vrg_kubectl $cluster_names apply
+}
+application_sample_vrg_undeploy()
+{
+	application_sample_vrg_kubectl $cluster_names delete\ --ignore-not-found
+}
+set -ex
 for command in "${@:-deploy}"; do
 	$command
 done
+{ set +x; } 2>/dev/null
 unset -v command
+unset -f application_sample_vrg_undeploy
+unset -f application_sample_vrg_deploy
+unset -f application_sample_vrg_kubectl
+unset -f application_sample_undeploy
+unset -f application_sample_deploy
+unset -f application_sample_kubectl
+unset -f application_sample_namespace_undeploy
+unset -f application_sample_namespace_deploy
+unset -v application_sample_namespace_name
 unset -f manager_redeploy
 unset -f undeploy
 unset -f deploy
