d2192fbe6925d6409865b9a50f60d302d6db883d
Raghavendra Talur <raghavendra.talur@gmail.com>
2022-07-07 15:35:47 -0400

    drcluster: disable funlen linter
    We need to refactor the reconcile function fully to reduce the line
    length. Disabling the funlen linter for this function for now.
    
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- controllers/drcluster_controller.go
+++ controllers/drcluster_controller.go
@@ -87,18 +87,10 @@ const (
 //+kubebuilder:rbac:groups=work.open-cluster-management.io,resources=manifestworks,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups="",resources=configmaps,verbs=list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DRCluster object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.9.2/pkg/reconcile
-// TODO: Validate managedCluster name? and also ensure it is not deleted!
-// TODO: Setup views for storage class and VRClass to read and report IDs
+//nolint:funlen
 func (r *DRClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	// TODO: Validate managedCluster name? and also ensure it is not deleted!
+	// TODO: Setup views for storage class and VRClass to read and report IDs
 	log := ctrl.Log.WithName("controllers").WithName("drcluster").WithValues("name", req.NamespacedName.Name)
 	log.Info("reconcile enter")
 
