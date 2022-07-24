32ca1fe31925c062af892b400ce65fff9f5dbb99
Shyamsundar Ranganathan <srangana@redhat.com>
2022-07-24 09:35:33 -0400

    Delay UserPl finalizer removal till DRPC is finalized
    Current order of finalizer addition is DRPC resource first,
    followed by UserPl. The order of removal is the reverse.
    
    During removal though, the finalizer on the UserPl is removed
    earlier than completion of finalizing DRPC.
    
    In case of any reconciles post initial start of deletion, the
    userPl would be garbage collected, and if DRPC is not deleted
    but the user placement is, then future reconciles will not
    continue the deletion process as isBeingDeleted will find neither
    the DRPC as being deleted, or the UserPl to continue processing
    deletion.
    
    This commit delays the removal of UserPl finalizer to after
    processing of DRPC for deletion, such that if the deletion was
    processed based on the UserPl deletion, it will complete the
    process and ensures the PlRule is not deleted till then.
    
    There is still a race, where post removal of the UserPl finalizer
    the update for DRPC finalizer removal fails, at which point a
    future reconcile will not pick up the fact that it is a delete
    operation, and would keep reporting UserPl missing. But, this
    does not leak any resource as such.
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- controllers/drplacementcontrol_controller.go
+++ controllers/drplacementcontrol_controller.go
@@ -566,6 +566,17 @@ func (r *DRPlacementControlReconciler) processDeletion(ctx context.Context,
 	drpc *rmn.DRPlacementControl, usrPlRule *plrv1.PlacementRule, log logr.Logger) (ctrl.Result, error) {
 	log.Info("Processing DRPC deletion")
 
+	if !controllerutil.ContainsFinalizer(drpc, DRPCFinalizer) {
+		return ctrl.Result{}, nil
+	}
+
+	// Run finalization logic for dprc.
+	// If the finalization logic fails, don't remove the finalizer so
+	// that we can retry during the next reconciliation.
+	if err := r.finalizeDRPC(ctx, drpc, log); err != nil {
+		return ctrl.Result{}, err
+	}
+
 	if usrPlRule != nil && controllerutil.ContainsFinalizer(usrPlRule, DRPCFinalizer) {
 		// Remove DRPCFinalizer from User PlacementRule.
 		controllerutil.RemoveFinalizer(usrPlRule, DRPCFinalizer)
@@ -576,24 +587,15 @@ func (r *DRPlacementControlReconciler) processDeletion(ctx context.Context,
 		}
 	}
 
-	if controllerutil.ContainsFinalizer(drpc, DRPCFinalizer) {
-		// Run finalization logic for dprc.
-		// If the finalization logic fails, don't remove the finalizer so
-		// that we can retry during the next reconciliation.
-		if err := r.finalizeDRPC(ctx, drpc, log); err != nil {
-			return ctrl.Result{}, err
-		}
+	// Remove DRPCFinalizer from DRPC.
+	controllerutil.RemoveFinalizer(drpc, DRPCFinalizer)
 
-		// Remove DRPCFinalizer from DRPC.
-		controllerutil.RemoveFinalizer(drpc, DRPCFinalizer)
-
-		err := r.Update(ctx, drpc)
-		if err != nil {
-			return ctrl.Result{}, fmt.Errorf("failed to update drpc %w", err)
-		}
-
-		r.Callback(drpc.Name, "deleted")
+	err := r.Update(ctx, drpc)
+	if err != nil {
+		return ctrl.Result{}, fmt.Errorf("failed to update drpc %w", err)
 	}
+
+	r.Callback(drpc.Name, "deleted")
 
 	return ctrl.Result{}, nil
 }
