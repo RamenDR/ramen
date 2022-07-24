9e2d030feb43a47d8319d22da71c3d575a2eb3a0
hatfieldbrian <bhatfiel@redhat.com>
2022-07-23 22:41:57 -0700

    kube-objects: Delete backup and restore kube objects after all sub operations complete
    	- use two backup numbers: 0 and 1
    	- delete backup s3 objects immediately before number reuse
    	- add the following to velero Kube object names:
    		- vrg namespace name
    		- vrg name
    		- objects group name or number
    		- s3 profile name
    	- remove kube objects groups wait loops
    	- define kube objects spec common to capture and recovery
    	- make all kube object group fields optional
    
    Signed-off-by: hatfieldbrian <bhatfiel@redhat.com>

--- hack/minikube-ramen.sh
+++ hack/minikube-ramen.sh
@@ -3,8 +3,7 @@
 set -e
 ramen_hack_directory_path_name=$(dirname $0)
 cluster_names=${cluster_names:-cluster1\ cluster2}
-deploy()
-{
+deploy() {
 	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube.sh minikube_start_spokes
 	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube-ramen.sh\
 		rook_ceph_deploy\
@@ -13,49 +12,51 @@ deploy()
 		ramen_deploy_spokes\
 
 }
-undeploy()
-{
+undeploy() {
 	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube-ramen.sh\
 		ramen_undeploy_spokes\
 		minio_undeploy_spokes\
 		rook_ceph_undeploy\
 
 }
-manager_redeploy()
-{
-	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube-ramen.sh\
-		ramen_undeploy_spokes\
-		ramen_manager_image_build_and_archive\
-		ramen_deploy_spokes\
-
+manager_image_build() {
+	$ramen_hack_directory_path_name/ocm-minikube-ramen.sh ramen_manager_image_build_and_archive
+}
+manager_image_deployed() {
 	for cluster_name in $cluster_names; do
 		minikube -p $cluster_name ssh -- docker images\|grep ramen
 	done; unset -v cluster_name
 }
+manager_deploy() {
+	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube-ramen.sh ramen_deploy_spokes
+	manager_image_deployed
+}
+manager_undeploy() {
+	spoke_cluster_names=$cluster_names $ramen_hack_directory_path_name/ocm-minikube-ramen.sh ramen_undeploy_spokes
+}
+manager_redeploy() {
+	manager_undeploy
+	manager_image_build
+	manager_deploy
+}
 application_sample_namespace_name=${application_sample_namespace_name:-default}
-application_sample_namespace_deploy()
-{
+application_sample_namespace_deploy() {
 	kubectl create namespace $application_sample_namespace_name --dry-run=client -oyaml|kubectl --context $1 apply -f-
 }
-application_sample_namespace_undeploy()
-{
+application_sample_namespace_undeploy() {
 	kubectl --context $1 delete namespace $application_sample_namespace_name
 }
-application_sample_kubectl()
-{
+application_sample_kubectl() {
 	kubectl --context $1 -n$application_sample_namespace_name $2 -khttps://github.com/RamenDR/ocm-ramen-samples/busybox
 }
-application_sample_deploy()
-{
+application_sample_deploy() {
 	application_sample_kubectl $1 apply
 }
-application_sample_undeploy()
-{
+application_sample_undeploy() {
 	application_sample_kubectl $1 delete
 }
-application_sample_vrg_kubectl()
-{
-	cat <<-a|kubectl --context $1 -n$application_sample_namespace_name $2 -f-
+application_sample_vrg_kubectl() {
+	cat <<-a|kubectl --context $1 -n$application_sample_namespace_name $3 -f-
 	---
 	apiVersion: ramendr.openshift.io/v1alpha1
 	kind: VolumeReplicationGroup
@@ -69,20 +70,21 @@ application_sample_vrg_kubectl()
 	  pvcSelector:
 	    matchLabels:
 	      appname: busybox
-	  replicationState: primary
+	  replicationState: $2
 	  s3Profiles:
 $(for cluster_name in $cluster_names; do echo \ \ -\ minio-on-$cluster_name; done; unset -v cluster_name)
 	  sync:
 	    mode: Disabled${vrg_appendix-}
 	a
 }
-application_sample_vrg_deploy()
-{
-	application_sample_vrg_kubectl $1 apply\ -oyaml
+application_sample_vrg_deploy() {
+	application_sample_vrg_kubectl $1 primary apply\ -oyaml
 }
-application_sample_vrg_undeploy()
-{
-	application_sample_vrg_kubectl $1 delete\ --ignore-not-found
+application_sample_vrg_deploy_sec() {
+	application_sample_vrg_kubectl $1 secondary apply\ -oyaml
+}
+application_sample_vrg_undeploy() {
+	application_sample_vrg_kubectl $1 primary delete\ --ignore-not-found
 }
 set -x
 for command in "${@:-deploy}"; do
@@ -100,6 +102,10 @@ unset -f application_sample_namespace_undeploy
 unset -f application_sample_namespace_deploy
 unset -v application_sample_namespace_name
 unset -f manager_redeploy
+unset -f manager_undeploy
+unset -f manager_deploy
+unset -f manager_image_deployed
+unset -f manager_image_build
 unset -f undeploy
 unset -f deploy
 unset -v cluster_names
--- hack/shio-demo.sh
+++ hack/shio-demo.sh
@@ -17,8 +17,10 @@ velero_secret_deploy cluster1
 velero_secret_deploy cluster2
 kubectl --context cluster1 -nvelero get bsl,backup,restore
 kubectl --context cluster2 -nvelero get bsl,backup,restore
-	mc alias set cluster2 $(minikube_minio_url cluster2) minio minio123
-mc tree cluster2
+	for cluster_name in $s3_store_cluster_names; do
+		mc alias set $cluster_name $(minikube_minio_url $cluster_name) minio minio123
+		mc tree $cluster_name
+	done; unset -v cluster_name
 }; exit_stack_push unset -f infra_deploy
 
 infra_undeploy() {
@@ -56,9 +58,7 @@ kubectl --context $1 -nasdf get cm,secret,deploy,rs,po,pvc
 }; exit_stack_push unset -f app_kube_objects_list
 
 app_undeploy() {
-kubectl --context $1        delete namespace asdf&
-	vrg_finalizer0_remove $1
-wait
+kubectl --context $1        delete namespace asdf
 }; exit_stack_push unset -f app_undeploy
 
 vrg_deploy() {
@@ -66,9 +66,21 @@ vrg_deploy() {
   kubeObjectProtection: {}' \
 	vrg_appendix='
   kubeObjectProtection:
-    captureInterval: 1m' \
-	cluster_names=cluster2 application_sample_namespace_name=asdf $ramen_hack_directory_path_name/minikube-ramen.sh  application_sample_vrg_deploy\ $1
+    captureInterval: 1m
+    recoverOrder:
+      - excludedResources:
+          - po
+          - rs
+          - deploy
+      - includedResources:
+          - deployments
+' \
+	cluster_names=$s3_store_cluster_names application_sample_namespace_name=asdf $ramen_hack_directory_path_name/minikube-ramen.sh application_sample_vrg_deploy$2\ $1
 }; exit_stack_push unset -f vrg_deploy
+
+vrg_demote() {
+	vrg_deploy $1 _sec
+}; exit_stack_push unset -f vrg_demote
 
 vrg_finalizer0_remove() {
 	true_if_exit_status_and_stderr 1 'Error from server (NotFound): volumereplicationgroups.ramendr.openshift.io "bb" not found' \
@@ -80,56 +92,71 @@ vrg_get() {
 }; exit_stack_push unset -f vrg_get
 
 vrg_get_s3() {
-	mc cp -q $(app_s3_object_name_prefix)v1alpha1.VolumeReplicationGroup/a /tmp/a.json.gz;gzip -df /tmp/a.json.gz;python -m json.tool </tmp/a.json
+	mc cp -q $(app_s3_object_name_prefix $1)v1alpha1.VolumeReplicationGroup/a /tmp/a.json.gz;gzip -df /tmp/a.json.gz;python -m json.tool </tmp/a.json
 }; exit_stack_push unset -f vrg_get_s3
 
 app_protect() {
 	vrg_deploy cluster1
 	vrg_get cluster1
 time kubectl --context cluster1 -nasdf wait vrg/bb --for condition=clusterdataprotected --timeout 2m
-	app_protection_info 0
+	app_protection_info ""
 }; exit_stack_push unset -f app_protect
 
 app_unprotect() {
-	cluster_names=cluster2 application_sample_namespace_name=asdf $ramen_hack_directory_path_name/minikube-ramen.sh application_sample_vrg_undeploy\ $1&
-	vrg_finalizer0_remove $1
-	wait
+	cluster_names=$s3_store_cluster_names application_sample_namespace_name=asdf $ramen_hack_directory_path_name/minikube-ramen.sh application_sample_vrg_undeploy\ $1
 	kubectl --context $1 -nasdf delete events --all
+	velero_kube_objects_list $1
+	s3_objects_list
 }; exit_stack_push unset -f app_unprotect
 
 app_recover() {
+	vrg_demote cluster1
 	kubectl create namespace asdf --dry-run=client -oyaml|kubectl --context cluster2 apply -f-
 	vrg_deploy cluster2
 time kubectl --context cluster2 -nasdf wait vrg/bb --for condition=clusterdataready --timeout 2m
 	app_kube_objects_list cluster2
 }; exit_stack_push unset -f app_recover
 
+app_velero_kube_object_name() {
+	echo asdf--bb--$1--0--minio-on-$2
+}; exit_stack_push unset -f app_velero_kube_object_name
+
 s3_objects_list() {
-mc tree cluster2
-mc ls cluster2 --recursive
+	for cluster_name in $s3_store_cluster_names; do
+		mc tree $cluster_name
+		mc ls $cluster_name --recursive
+	done; unset -v cluster_name
 }; exit_stack_push unset -f s3_objects_list
 
 app_s3_object_name_prefix() {
-	echo cluster2/bucket/asdf/bb/
+	echo $1/bucket/asdf/bb/
 }; exit_stack_push unset -f app_s3_object_name_prefix
 
 app_s3_object_name_prefix_velero() {
-	echo $(app_s3_object_name_prefix)velero/
+	echo $(app_s3_object_name_prefix $2)kube-objects/$1/velero/
 }; exit_stack_push unset -f app_s3_object_name_prefix_velero
 
 app_s3_objects_delete() {
-mc rm $(app_s3_object_name_prefix) --recursive --force
+	for cluster_name in $s3_store_cluster_names; do
+		mc rm $(app_s3_object_name_prefix $cluster_name) --recursive --force
+	done; unset -v cluster_name
 }; exit_stack_push unset -f app_objects_delete
 
 app_protection_info() {
-mc cp -q $(app_s3_object_name_prefix_velero)backups/$1/velero-backup.json /tmp/$1-velero-backup.json;python -m json.tool </tmp/$1-velero-backup.json
-mc cp -q $(app_s3_object_name_prefix_velero)backups/$1/$1-resource-list.json.gz /tmp;gzip -df /tmp/$1-resource-list.json.gz;python -m json.tool </tmp/$1-resource-list.json
-mc cp -q $(app_s3_object_name_prefix_velero)backups/$1/$1-logs.gz /tmp;gzip -df /tmp/$1-logs.gz;cat /tmp/$1-logs
+	for cluster_name in $s3_store_cluster_names; do
+		set -- "$1" $(app_velero_kube_object_name "$1" $cluster_name) $(app_s3_object_name_prefix_velero "$1" $cluster_name)
+		mc cp -q $3backups/$2/velero-backup.json /tmp/$2-velero-backup.json;python -m json.tool </tmp/$2-velero-backup.json
+		mc cp -q $3backups/$2/$2-resource-list.json.gz /tmp;gzip -df /tmp/$2-resource-list.json.gz;python -m json.tool </tmp/$2-resource-list.json
+		mc cp -q $3backups/$2/$2-logs.gz /tmp;gzip -df /tmp/$2-logs.gz;cat /tmp/$2-logs
+	done; unset -v cluster_name
 }; exit_stack_push unset -f app_protection_info
 
 app_recovery_info() {
-mc cp -q $(app_s3_object_name_prefix_velero)restores/$1/restore-$1-results.gz /tmp;gzip -df /tmp/restore-$1-results.gz;python -m json.tool </tmp/restore-$1-results
-mc cp -q $(app_s3_object_name_prefix_velero)restores/$1/restore-$1-logs.gz /tmp;gzip -df /tmp/restore-$1-logs.gz;cat /tmp/restore-$1-logs
+	for cluster_name in $s3_store_cluster_names; do
+		set -- "$1" $(app_velero_kube_object_name "$1" $cluster_name) $(app_s3_object_name_prefix_velero "$1" $cluster_name)
+		mc cp -q $3restores/$2/restore-$2-results.gz /tmp;gzip -df /tmp/restore-$2-results.gz;python -m json.tool </tmp/restore-$2-results
+		mc cp -q $3restores/$2/restore-$2-logs.gz /tmp;gzip -df /tmp/restore-$2-logs.gz;cat /tmp/restore-$2-logs
+	done; unset -v cluster_name
 }; exit_stack_push unset -f app_recovery_info
 
 velero_kube_objects_list() {
@@ -150,6 +177,8 @@ velero_kube_objects_delete() {
 	kubectl --context $1 -n velero delete --all restores,backups,backupstoragelocations
 }; exit_stack_push unset -f velero_kube_objects_delete
 
+s3_store_cluster_names=${s3_store_cluster_names-cluster2\ cluster1}
+exit_stack_push unset -v s3_store_cluster_names
 exit_stack_push unset -v command
 set -x
 for command in "${@:-infra_deploy app_deploy app_protect}"; do
