61de4af89b7d8d918fcf58e11ec98e939f346633
rakeshgm <rakeshgm@redhat.com>
2022-11-01 17:17:34 +0530

    added webhook validation for DRPolicy
    Signed-off-by: rakeshgm <rakeshgm@redhat.com>

--- main.go
+++ main.go
@@ -177,6 +177,12 @@ func setupReconcilersHub(mgr ctrl.Manager) {
 		setupLog.Error(err, "unable to create webhook", "webhook", "DRCluster")
 		os.Exit(1)
 	}
+
+	// setup webhook for DRPolicy
+	if err := (&ramendrv1alpha1.DRPolicy{}).SetupWebhookWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create webhook", "webhook", "DRPolicy")
+		os.Exit(1)
+	}
 }
 
 func main() {
