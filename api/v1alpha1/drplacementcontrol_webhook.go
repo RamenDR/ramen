d7650d45a37271e9582ce5823c79c623fd9587e0
rakeshgm <rakeshgm@redhat.com>
2022-11-24 15:31:47 +0530

    adds webhook for drpc
    Signed-off-by: rakeshgm <rakeshgm@redhat.com>

---
+++ api/v1alpha1/drplacementcontrol_webhook.go
@@ -0,0 +1,67 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	"fmt"
+	"reflect"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/webhook"
+)
+
+// log is for logging in this package.
+var drplacementcontrollog = logf.Log.WithName("drplacementcontrol-resource")
+
+func (r *DRPlacementControl) SetupWebhookWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewWebhookManagedBy(mgr).
+		For(r).
+		Complete()
+}
+
+//nolint
+//+kubebuilder:webhook:path=/validate-ramendr-openshift-io-v1alpha1-drplacementcontrol,mutating=false,failurePolicy=fail,sideEffects=None,groups=ramendr.openshift.io,resources=drplacementcontrols,verbs=update,versions=v1alpha1,name=vdrplacementcontrol.kb.io,admissionReviewVersions=v1
+
+var _ webhook.Validator = &DRPlacementControl{}
+
+// ValidateCreate implements webhook.Validator so a webhook will be registered for the type
+func (r *DRPlacementControl) ValidateCreate() error {
+	drplacementcontrollog.Info("validate create", "name", r.Name)
+
+	return nil
+}
+
+// ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
+func (r *DRPlacementControl) ValidateUpdate(old runtime.Object) error {
+	drplacementcontrollog.Info("validate update", "name", r.Name)
+
+	oldDRPC, ok := old.(*DRPlacementControl)
+	if !ok {
+		return fmt.Errorf("error casting old DRPC")
+	}
+
+	// checks for immutability
+	if !reflect.DeepEqual(r.Spec.PlacementRef, oldDRPC.Spec.PlacementRef) {
+		return fmt.Errorf("PlacementRef cannot be changed")
+	}
+
+	if !reflect.DeepEqual(r.Spec.DRPolicyRef, oldDRPC.Spec.DRPolicyRef) {
+		return fmt.Errorf("DRPolicyRef cannot be changed")
+	}
+
+	if !reflect.DeepEqual(r.Spec.PVCSelector, oldDRPC.Spec.PVCSelector) {
+		return fmt.Errorf("PVCSelector cannot be changed")
+	}
+
+	return nil
+}
+
+// ValidateDelete implements webhook.Validator so a webhook will be registered for the type
+func (r *DRPlacementControl) ValidateDelete() error {
+	drplacementcontrollog.Info("validate delete", "name", r.Name)
+
+	return nil
+}
