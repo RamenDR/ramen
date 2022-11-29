91f2c865764ff39f19009470a813970b7b90916a
Nir Soffer <nsoffer@redhat.com>
2022-11-29 22:34:57 +0200

    Add --name-prefix option
    When specified, prefix environment name, profiles names, workers names,
    and reference to profile names in scripts arguments with the specified
    prefix. Prefixing the environment name and worker names is not required
    but it creates more consistent logs.
    
    This allows running multiple environment with the same profile names
    (e.g. dr1) at the same time.
    
    Templates names are not prefixed since they are not used in runtime.
    
    We do a detailed test with embedded test yaml in envfile_test.py. The
    actual environments have only basic test ensuring that loading with name
    prefix does not raise.
    
    Fixes: #637
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/ocm_test.py
+++ test/ocm_test.py
@@ -7,3 +7,8 @@ from drenv import envfile
 def test_load():
     with open("ocm.yaml") as f:
         envfile.load(f)
+
+
+def test_load_prefix():
+    with open("ocm.yaml") as f:
+        envfile.load(f, name_prefix="test-")
--- test/regional_dr_test.py
+++ test/regional_dr_test.py
@@ -7,3 +7,8 @@ from drenv import envfile
 def test_load():
     with open("regional-dr.yaml") as f:
         envfile.load(f)
+
+
+def test_load_prefix():
+    with open("regional-dr.yaml") as f:
+        envfile.load(f, name_prefix="test-")
