9e2a6b9071e78726e7990bfebc4ab1412d3b7aaa
Nir Soffer <nsoffer@redhat.com>
2022-12-06 23:27:19 +0200

    Introduce commands.watch()
    Add a helper to handle the common case in drenv; running a command and
    logging its lines read from the child process stdout.
    
    In the current code we redirect stderr to stdout, which makes it hard to
    find the error message when a command fails. This helper avoids the
    issue by reading both stdout and stderror at the same time. Text read
    from stderr is buffered while the caller iterate over the lines read
    from stdout.
    
    Example usage:
    
        for line in commands.watch("minikube", "start"):
            logging.debug("%s", line)
    
    If the command fails, an error is raised including the buffered error
    text:
    
        commands.Error: Command failed:
           command: ("minikube", "start")
           exitcode: 1
           error:
              Error message from minikube
    
    Part-of: #648
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/drenv/commands.py
+++ test/drenv/commands.py
@@ -3,11 +3,84 @@
 
 import os
 import selectors
+import subprocess
+import textwrap
 
 OUT = "out"
 ERR = "err"
 
 _Selector = getattr(selectors, "PollSelector", selectors.SelectSelector)
+
+
+class Error(Exception):
+    INDENT = 3 * " "
+
+    def __init__(self, command, exitcode, error):
+        self.command = command
+        self.exitcode = exitcode
+        self.error = error
+
+    def __str__(self):
+        error = textwrap.indent(self.error.rstrip(), self.INDENT * 2)
+        return f"""\
+Command failed:
+   command: {self.command}
+   exitcode: {self.exitcode}
+   error:
+{error}
+"""
+
+
+def watch(*args):
+    """
+    Run command args, iterating over lines read from the child process stdout.
+
+    Assumes that the child process output UTF-8. Will raise if the command
+    outputs binary data. This is not a problem in this projects since all our
+    commands are text based.
+
+    Invalid UTF-8 in child process stderr is handled gracefully so we don't
+    fail to raise an error about the failing command. Invalid characters will
+    be replaced with unicode replacement character (U+FFFD).
+
+    Raises Error if the child process terminated with non-zero exit code. The
+    error includes all data read from the child process stderr.
+    """
+    # Avoid delays in python child process logs.
+    env = dict(os.environ)
+    env["PYTHONUNBUFFERED"] = "1"
+
+    with subprocess.Popen(
+        args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=env,
+    ) as p:
+        error = bytearray()
+        partial = bytearray()
+
+        for src, data in stream(p):
+            if src is ERR:
+                error += data
+            else:
+                for line in data.splitlines(keepends=True):
+                    if not line.endswith(b"\n"):
+                        partial += line
+                        break
+
+                    if partial:
+                        line = bytes(partial) + line
+                        del partial[:]
+
+                    yield line.rstrip().decode()
+
+        if partial:
+            yield partial.rstrip().decode()
+            del partial[:]
+
+    if p.returncode != 0:
+        error = error.decode(errors="replace")
+        raise Error(args, p.returncode, error)
 
 
 def stream(proc, bufsize=32 << 10):
