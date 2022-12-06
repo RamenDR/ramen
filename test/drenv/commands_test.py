8694d4596b7e4b2ae4e3c65a35081650e971a450
Nir Soffer <nsoffer@redhat.com>
2022-12-06 23:27:19 +0200

    Introduce commands.watch()
    Add a helper to handle the common case in drenv; running a command and
    logging its lines read from the child process stdout.
    
    In the current code we redirect stderr to stdout, which makes it hard to
    find the error message when a command fails. This helper avoids the
    issue by reading both stdout and stderror at the same time. Text read
    from stderr is buffered while the caller iterate over the lines read
    from stdout.
    
    Example usage:
    
        for line in commands.watch("minikube", "start"):
            logging.debug("%s", line)
    
    If the command fails, an error is raised including the buffered error
    text:
    
        commands.Error: Command failed:
           command: ("minikube", "start")
           exitcode: 1
           error:
              Error message from minikube
    
    Part-of: #648
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/drenv/commands_test.py
+++ test/drenv/commands_test.py
@@ -4,7 +4,11 @@
 import subprocess
 from contextlib import contextmanager
 
+import pytest
+
 from drenv import commands
+
+# Streaming data from commands.
 
 
 def test_stream_nothing():
@@ -62,6 +66,111 @@ def test_stream_no_stdout_stderr():
     with run("true", stdout=None, stderr=None) as p:
         stream = list(commands.stream(p))
     assert stream == []
+
+
+# Watching commands.
+
+
+def test_watch_no_output():
+    output = list(commands.watch("true"))
+    assert output == []
+
+
+def test_watch_lines():
+    script = """
+for i in range(10):
+    print(f"line {i}", flush=True)
+"""
+    output = list(commands.watch("python3", "-c", script))
+    assert output == ["line %d" % i for i in range(10)]
+
+
+def test_watch_partial_lines():
+    script = """
+import time
+
+print("first ", end="", flush=True);
+time.sleep(0.02)
+print("line\\nsecond ", end="", flush=True);
+time.sleep(0.02)
+print("line\\n", end="", flush=True);
+"""
+    output = list(commands.watch("python3", "-c", script))
+    assert output == ["first line", "second line"]
+
+
+def test_watch_no_newline():
+    script = """
+import time
+
+print("first ", end="", flush=True);
+time.sleep(0.02)
+print("second ", end="", flush=True);
+time.sleep(0.02)
+print("last", end="", flush=True);
+"""
+    output = list(commands.watch("python3", "-c", script))
+    assert output == ["first second last"]
+
+
+def test_watch_error_empty():
+    cmd = ("false",)
+    output = []
+
+    with pytest.raises(commands.Error) as e:
+        for line in commands.watch(*cmd):
+            output.append(line)
+
+    assert output == []
+
+    assert e.value.command == cmd
+    assert e.value.exitcode == 1
+    assert e.value.error == ""
+
+
+def test_watch_error():
+    cmd = ("sh", "-c", "echo -n output >&1; echo -n error >&2; exit 1")
+    output = []
+
+    with pytest.raises(commands.Error) as e:
+        for line in commands.watch(*cmd):
+            output.append(line)
+
+    # All output is always reported to the caller, even if the command failed.
+    assert output == ["output"]
+
+    # Errors are buffered and reported only if the command failed.
+    assert e.value.command == cmd
+    assert e.value.exitcode == 1
+    assert e.value.error == "error"
+
+
+def test_watch_non_ascii():
+    script = 'print("\u05d0")'  # Hebrew Letter Alef (U+05D0)
+    output = list(commands.watch("python3", "-c", script))
+    assert output == ["\u05d0"]
+
+
+def test_watch_invalid_utf8():
+    script = """
+import os
+os.write(1, bytes([0xff]))
+"""
+    with pytest.raises(UnicodeDecodeError):
+        list(commands.watch("python3", "-c", script))
+
+
+def test_error():
+    e = commands.Error(("arg1", "arg2"), exitcode=2, error="err 1\nerr 2\n")
+    expected = """\
+Command failed:
+   command: ('arg1', 'arg2')
+   exitcode: 2
+   error:
+      err 1
+      err 2
+"""
+    assert str(e) == expected
 
 
 @contextmanager
