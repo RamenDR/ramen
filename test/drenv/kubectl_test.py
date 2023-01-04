49629211f8469e517ccbc83bbe9fd72bfbcb9cec
Nir Soffer <nsoffer@redhat.com>
2023-01-04 19:31:39 +0200

    Test the kubectl module
    Add integration tests for the kubectl module, using the tmpenv fixture.
    
    Part-of: #677
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ test/drenv/kubectl_test.py
@@ -0,0 +1,74 @@
+# SPDX-FileCopyrightText: The RamenDR authors
+# SPDX-License-Identifier: Apache-2.0
+
+import json
+
+from drenv import kubectl
+
+
+def test_get(tmpenv):
+    out = kubectl.get("deploy", "--output=name", profile=tmpenv.profile)
+    assert out.strip() == "deployment.apps/example-deployment"
+
+
+def test_config(tmpenv):
+    out = kubectl.config("view", "--output=json")
+    json.loads(out)
+
+
+def test_exec(tmpenv):
+    out = kubectl.exec(
+        "deploy/example-deployment",
+        "--",
+        "hostname",
+        profile=tmpenv.profile,
+    )
+    assert out.startswith("example-deployment-")
+
+
+def test_apply(tmpenv, capsys):
+    kubectl.apply("--filename=example/deployment.yaml", profile=tmpenv.profile)
+    out, err = capsys.readouterr()
+    assert out.strip() == "deployment.apps/example-deployment unchanged"
+
+
+def test_rollout(tmpenv, capsys):
+    kubectl.rollout(
+        "status",
+        "deploy/example-deployment",
+        "--timeout=10s",
+        profile=tmpenv.profile,
+    )
+    out, err = capsys.readouterr()
+    assert out.strip() == 'deployment "example-deployment" successfully rolled out'
+
+
+def test_wait(tmpenv, capsys):
+    kubectl.wait(
+        "deploy/example-deployment",
+        "--for=condition=available",
+        "--timeout=10s",
+        profile=tmpenv.profile,
+    )
+    out, err = capsys.readouterr()
+    assert out.strip() == "deployment.apps/example-deployment condition met"
+
+
+def test_patch(tmpenv, capsys):
+    pod = kubectl.get("pod", "--output=name", profile=tmpenv.profile).strip()
+    kubectl.patch(
+        pod,
+        "--type=merge",
+        '--patch={"metadata": {"labels": {"test": "yes"}}}',
+        profile=tmpenv.profile,
+    )
+    out, err = capsys.readouterr()
+    assert out.strip() == f"{pod} patched"
+
+
+def test_delete(tmpenv, capsys):
+    pod = kubectl.get("pod", "--output=name", profile=tmpenv.profile).strip()
+    kubectl.delete(pod, profile=tmpenv.profile)
+    out, err = capsys.readouterr()
+    _, name = pod.split("/", 1)
+    assert out.strip() == f'pod "{name}" deleted'
