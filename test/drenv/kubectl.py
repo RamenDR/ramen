ad06c6da8dacd3e464ce046db9a96d38389ace6e
Nir Soffer <nsoffer@redhat.com>
2023-02-07 18:38:29 +0200

    Rename profile to context in kubectl
    In drenv we use the term `profile`, matching `minikube`. In `kubectl`
    this is called `context`. Rename the argument to `kubectl` commands to
    match `kubectl`.
    
    `drenv.wait_for` is still using the term `profile`, since it is part of
    the `drenv` package. This is little bit confusing in code using both
    `kubectl.wait` and `drenv.wait_for`. I think the best solution for this
    is to move `drenv.wait_for` into `kubectl.wait`.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/drenv/kubectl.py
+++ test/drenv/kubectl.py
@@ -4,74 +4,74 @@
 from . import commands
 
 
-def config(*args, profile=None):
+def config(*args, context=None):
     """
     Run kubectl config ... and return the output.
     """
-    return _run("config", *args, profile=profile)
+    return _run("config", *args, context=context)
 
 
-def get(*args, profile=None):
+def get(*args, context=None):
     """
     Run kubectl get ... and return the output.
     """
-    return _run("get", *args, profile=profile)
+    return _run("get", *args, context=context)
 
 
-def exec(*args, profile=None):
+def exec(*args, context=None):
     """
     Run kubectl get ... and return the output.
     """
-    return _run("exec", *args, profile=profile)
+    return _run("exec", *args, context=context)
 
 
-def apply(*args, input=None, profile=None):
+def apply(*args, input=None, context=None):
     """
     Run kubectl apply ... logging progress messages.
     """
-    _watch("apply", *args, input=input, profile=profile)
+    _watch("apply", *args, input=input, context=context)
 
 
-def patch(*args, profile=None):
+def patch(*args, context=None):
     """
     Run kubectl patch ... logging progress messages.
     """
-    _watch("patch", *args, profile=profile)
+    _watch("patch", *args, context=context)
 
 
-def delete(*args, input=None, profile=None):
+def delete(*args, input=None, context=None):
     """
     Run kubectl delete ... logging progress messages.
     """
-    _watch("delete", *args, input=input, profile=profile)
+    _watch("delete", *args, input=input, context=context)
 
 
-def rollout(*args, profile=None):
+def rollout(*args, context=None):
     """
     Run kubectl rollout ... logging progress messages.
     """
-    _watch("rollout", *args, profile=profile)
+    _watch("rollout", *args, context=context)
 
 
-def wait(*args, profile=None):
+def wait(*args, context=None):
     """
     Run kubectl wait ... logging progress messages.
     """
-    _watch("wait", *args, profile=profile)
+    _watch("wait", *args, context=context)
 
 
-def _run(cmd, *args, profile=None):
+def _run(cmd, *args, context=None):
     cmd = ["kubectl", cmd]
-    if profile:
-        cmd.extend(("--context", profile))
+    if context:
+        cmd.extend(("--context", context))
     cmd.extend(args)
     return commands.run(*cmd)
 
 
-def _watch(cmd, *args, input=None, profile=None):
+def _watch(cmd, *args, input=None, context=None):
     cmd = ["kubectl", cmd]
-    if profile:
-        cmd.extend(("--context", profile))
+    if context:
+        cmd.extend(("--context", context))
     cmd.extend(args)
     for line in commands.watch(*cmd, input=input):
         print(line)
