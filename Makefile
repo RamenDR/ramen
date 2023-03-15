RamenDR/ramen

commit b8617d8d8c47157b2240d61bb22d15ff3d077b8d

Update envtest setup to latest
Change as per operator-sdk current practices,
- Exact change as per SDK upgrade process is not
known, but has changed since 1.8.0

Also update envtest to cancel the controller context
before shutdown

Change triggered due to investigating:
https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.14.0/#upgrade-k8s-versions-to-use-122-golangv3

Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

Makefile
@@ -136,11 +136,14 @@ lint: golangci-bin ## Run configured golangci-lint and pre-commit.sh linters aga
 	testbin/golangci-lint run ./... --config=./.golangci.yaml
 	hack/pre-commit.sh
 
+ENVTEST_K8S_VERSION = 1.25.0
 ENVTEST_ASSETS_DIR=$(shell pwd)/testbin
-setup-envtest:
-	mkdir -p ${ENVTEST_ASSETS_DIR}
-	test -f ${ENVTEST_ASSETS_DIR}/setup-envtest.sh || curl -sSLo ${ENVTEST_ASSETS_DIR}/setup-envtest.sh https://raw.githubusercontent.com/kubernetes-sigs/controller-runtime/v0.8.3/hack/setup-envtest.sh
-	source ${ENVTEST_ASSETS_DIR}/setup-envtest.sh; fetch_envtest_tools $(ENVTEST_ASSETS_DIR); setup_envtest_env $(ENVTEST_ASSETS_DIR)
+envtest:
+	mkdir -p $(ENVTEST_ASSETS_DIR)
+	test -s $(ENVTEST_ASSETS_DIR)/setup-envtest || GOBIN=$(ENVTEST_ASSETS_DIR) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
+
+setup-envtest: envtest
+	KUBEBUILDER_ASSETS="$(shell $(ENVTEST_ASSETS_DIR)/setup-envtest use $(ENVTEST_K8S_VERSION) --bin-dir $(ENVTEST_ASSETS_DIR) -p path)"
 
 test: generate manifests setup-envtest ## Run tests.
 	go test ./... -coverprofile cover.out $(GO_TEST_GINKGO_ARGS)

controllers/drpolicy_controller_test.go
@@ -266,9 +266,8 @@ var _ = Describe("DrpolicyController", func() {
 	})
 	When(`a drpolicy creation request contains an invalid scheduling interval`, func() {
 		It(`should fail`, func() {
-			err := func() *errors.StatusError {
+			err := func(value string) *errors.StatusError {
 				path := field.NewPath(`spec`, `schedulingInterval`)
-				value := ``
 
 				return errors.NewInvalid(
 					schema.GroupKind{
@@ -289,12 +288,12 @@ var _ = Describe("DrpolicyController", func() {
 						),
 					},
 				)
-			}()
+			}
 			drp := drpolicy.DeepCopy()
 			drp.Spec.SchedulingInterval = `3s`
-			Expect(k8sClient.Create(context.TODO(), drp)).To(MatchError(err))
+			Expect(k8sClient.Create(context.TODO(), drp)).To(MatchError(err(drp.Spec.SchedulingInterval)))
 			drp.Spec.SchedulingInterval = `0`
-			Expect(k8sClient.Create(context.TODO(), drp)).To(MatchError(err))
+			Expect(k8sClient.Create(context.TODO(), drp)).To(MatchError(err(drp.Spec.SchedulingInterval)))
 		})
 	})
 
@@ -304,7 +303,6 @@ var _ = Describe("DrpolicyController", func() {
 			drp.Spec.DRClusters = nil
 			err := func() *errors.StatusError {
 				path := field.NewPath("spec", "drClusters")
-				value := "null"
 
 				return errors.NewInvalid(
 					schema.GroupKind{
@@ -313,15 +311,9 @@ var _ = Describe("DrpolicyController", func() {
 					},
 					drp.Name,
 					field.ErrorList{
-						field.Invalid(
+						field.Required(
 							path,
-							value,
-							validationErrors.InvalidType(
-								path.String(),
-								"body",
-								"array",
-								value,
-							).Error(),
+							"",
 						),
 					},
 				)

controllers/suite_test.go
@@ -25,7 +25,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	controller_runtime_config "sigs.k8s.io/controller-runtime/pkg/config/v1alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/envtest"
-	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
@@ -58,6 +57,8 @@ var (
 	apiReader   client.Reader
 	k8sClient   client.Client
 	testEnv     *envtest.Environment
+	ctx         context.Context
+	cancel      context.CancelFunc
 	configMap   *corev1.ConfigMap
 	ramenConfig *ramendrv1alpha1.RamenConfig
 	testLogger  logr.Logger
@@ -79,9 +80,7 @@ var (
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
 
-	RunSpecsWithDefaultAndCustomReporters(t,
-		"Controller Suite",
-		[]Reporter{printer.NewlineReporter{}})
+	RunSpecs(t, "Controller Suite")
 }
 
 func namespaceCreate(name string) {
@@ -115,7 +114,7 @@ var _ = BeforeSuite(func() {
 	ramencontrollers.ControllerType = ramendrv1alpha1.DRHubType
 
 	if _, set := os.LookupEnv("KUBEBUILDER_ASSETS"); !set {
-		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../testbin/bin")).To(Succeed())
+		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../testbin/k8s/1.25.0-linux-amd64")).To(Succeed())
 	}
 
 	rNs, set := os.LookupEnv("POD_NAMESPACE")
@@ -341,8 +340,9 @@ var _ = BeforeSuite(func() {
 	err = drpcReconciler.SetupWithManager(k8sManager)
 	Expect(err).ToNot(HaveOccurred())
 
+	ctx, cancel = context.WithCancel(context.TODO())
 	go func() {
-		err = k8sManager.Start(ctrl.SetupSignalHandler())
+		err = k8sManager.Start(ctx)
 		Expect(err).ToNot(HaveOccurred())
 	}()
 
@@ -354,6 +354,7 @@ var _ = BeforeSuite(func() {
 }, 60)
 
 var _ = AfterSuite(func() {
+	cancel()
 	Expect(k8sClient.Delete(context.TODO(), configMap)).To(Succeed())
 	By("tearing down the test environment")
 	err := testEnv.Stop()

controllers/util/util_suite_test.go
@@ -57,7 +57,7 @@ var _ = BeforeSuite(func() {
 
 	By("Setting up KUBEBUILDER_ASSETS for envtest")
 	if _, set := os.LookupEnv("KUBEBUILDER_ASSETS"); !set {
-		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../../testbin/bin")).To(Succeed())
+		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../../testbin/k8s/1.25.0-linux-amd64")).To(Succeed())
 	}
 
 	By("Bootstrapping test environment")

controllers/volsync/volsync_suite_test.go
@@ -74,7 +74,7 @@ var _ = BeforeSuite(func() {
 
 	By("Setting up KUBEBUILDER_ASSETS for envtest")
 	if _, set := os.LookupEnv("KUBEBUILDER_ASSETS"); !set {
-		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../../testbin/bin")).To(Succeed())
+		Expect(os.Setenv("KUBEBUILDER_ASSETS", "../../testbin/k8s/1.25.0-linux-amd64")).To(Succeed())
 	}
 
 	By("bootstrapping test environment")

commit 92835af9cdcb7b76e2d57b04799879aa45953199

Fix handling of KUBEBUILDER_ASSETS
When we switch to operator-sdk 1.24.0 in commit b8617d8d8c47 (Update
envtest setup to latest) as `setup-envtest` target was added. We did not
understand why this target was needed, but the build was failing without
it.

In commit e6660972a765 (Change the hardcoded KUBEBUILDER_ASSETS to use
dynamically detected os/arch) we discovered that tests are failing on
macOS/aarch64, and patched it quickly to unlock people on macOS, but did
not fix the underlying issue.

The reason the tests started to fail is that KUBEBUILDER_ASSETS variable
was added, but it was not passed to the `go test` command. Looking into
a new project created using latest operator-sdk, this variable is passed
to `go test`, and this was probably the intent of commit b8617d8d8c47.

In new project created by operator-sdk this variable cannot be
overridden by the caller, but Shyam suggests that we want to make it
possible to override it like other environment variables.

Fix the issue by:

1. Introduce KUBEBUILDER_ASSETS optional variable. If this variable is
   defined, we used the defined value. If not, we run setup-envtest to
   download test executables and pass the standard path
   (testbin/k8s/version-os-arch) to `go test`.

2. Pass KUBEBUILDER_ASSETS to all `go test` invocations, like new
   operator-sdk project does. This will make it easy to upgrade operator
   sdk in the future.

Normal build did not change - we create the standard test env in
testbin. But now you can create another test environment, and test the
code with 2 versions.

Here is example run showing how it works:

    # Remove current test env:
    $ chmod -R +w testbin/k8s
    $ rm -rf testbin/k8s

    # Create a another test env using latest:
    $ testbin/setup-envtest use --bin-dir /home/nsoffer/tmp/envtest --print path latest

    # Tests using new test env:
    $ make test KUBEBUILDER_ASSETS=/home/nsoffer/tmp/envtest/k8s/1.26.1-linux-amd64
    ...
    KUBEBUILDER_ASSETS=/home/nsoffer/tmp/envtest/k8s/1.26.1-linux-amd64 go test ./... -coverprofile cover.out -test.v -ginkgo.v -ginkgo.failFast

    # The standard test env was not created:
    $ tree testbin/
    testbin/
    └── setup-envtest

    1 directory, 1 file

    # Running make test normally:
    $ make test
    ...
    KUBEBUILDER_ASSETS=/home/nsoffer/src/ramen/testbin/k8s/1.25.0-linux-amd64 go test ./... -coverprofile cover.out -test.v -ginkgo.v -ginkgo.failFast

    # The standard env was created:
    $ tree testbin
    testbin
    ├── k8s
    │   └── 1.25.0-linux-amd64
    │       ├── etcd
    │       ├── kube-apiserver
    │       └── kubectl
    └── setup-envtest

    3 directories, 4 files

Fixes: b8617d8d8c47 (Update envtest setup to latest)

Makefile
@@ -93,6 +93,11 @@ GO_TEST_GINKGO_ARGS ?= -test.v -ginkgo.v -ginkgo.failFast
 
 DOCKERCMD ?= podman
 
+ENVTEST_K8S_VERSION = 1.25.0
+ENVTEST_ASSETS_DIR = $(shell pwd)/testbin
+# Define to override the path to envtest executables.
+KUBEBUILDER_ASSETS ?= $(shell $(ENVTEST_ASSETS_DIR)/setup-envtest use $(ENVTEST_K8S_VERSION) --bin-dir $(ENVTEST_ASSETS_DIR) --print path)
+
 all: build
 
 ##@ General
@@ -136,38 +141,33 @@ lint: golangci-bin ## Run configured golangci-lint and pre-commit.sh linters aga
 	testbin/golangci-lint run ./... --config=./.golangci.yaml
 	hack/pre-commit.sh
 
-ENVTEST_K8S_VERSION = 1.25.0
-ENVTEST_ASSETS_DIR=$(shell pwd)/testbin
 envtest:
 	mkdir -p $(ENVTEST_ASSETS_DIR)
 	test -s $(ENVTEST_ASSETS_DIR)/setup-envtest || GOBIN=$(ENVTEST_ASSETS_DIR) go install sigs.k8s.io/controller-runtime/tools/setup-envtest@latest
 
-setup-envtest: envtest
-	KUBEBUILDER_ASSETS="$(shell $(ENVTEST_ASSETS_DIR)/setup-envtest use $(ENVTEST_K8S_VERSION) --bin-dir $(ENVTEST_ASSETS_DIR) -p path)"
+test: generate manifests envtest ## Run tests.
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./... -coverprofile cover.out $(GO_TEST_GINKGO_ARGS)
 
-test: generate manifests setup-envtest ## Run tests.
-	go test ./... -coverprofile cover.out $(GO_TEST_GINKGO_ARGS)
+test-pvrgl: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus ProtectedVolumeReplicationGroupList
 
-test-pvrgl: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus ProtectedVolumeReplicationGroupList
+test-vrg: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroup
 
-test-vrg: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroup
+test-vrg-vr: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroupVolRep
 
-test-vrg-vr: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroupVolRep
+test-vrg-vs: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroupVolSync
 
-test-vrg-vs: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VolumeReplicationGroupVolSync
+test-vrg-kubeobjects: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VRG_KubeObjectProtection
 
-test-vrg-kubeobjects: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus VRG_KubeObjectProtection
+test-drpc: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus DRPlacementControl
 
-test-drpc: generate manifests setup-envtest
-	go test ./controllers -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus DRPlacementControl
-
-test-util-pvc: generate manifests setup-envtest
-	go test ./controllers/util -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus PVCS_Util
+test-util-pvc: generate manifests envtest
+	KUBEBUILDER_ASSETS=$(KUBEBUILDER_ASSETS) go test ./controllers/util -coverprofile cover.out $(GO_TEST_GINKGO_ARGS) -ginkgo.focus PVCS_Util
 
 test-drenv:
 	$(MAKE) -C test