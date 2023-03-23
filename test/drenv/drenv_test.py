6e87412e8508760f365f6808b26996e314f4c9b9
Shwetha K Acharya <sacharya@redhat.com>
2023-03-23 22:13:13 +0530

    drenv: Fix broken drenv dump
    drenv cmd_dump() errors out with the following message:
    cmd_dump() takes 1 positional argument but 2 were given
    
    - Passed args to cmd_dump function to resolve above issue.
    - Added missing drenv dump tests
    - Added external PR link to external.yaml better understanding
    
    Signed-off-by: Shwetha K Acharya <sacharya@redhat.com>

--- test/drenv/drenv_test.py
+++ test/drenv/drenv_test.py
@@ -1,6 +1,7 @@
 # SPDX-FileCopyrightText: The RamenDR authors
 # SPDX-License-Identifier: Apache-2.0
 
+import yaml
 import pytest
 
 from drenv import cluster
@@ -16,6 +17,20 @@ def test_start_unknown():
 def test_start(tmpenv):
     commands.run("drenv", "start", "--name-prefix", tmpenv.prefix, "external.yaml")
     assert cluster.status(tmpenv.prefix + "cluster") == cluster.READY
+
+
+def test_dump_without_prefix():
+    out = commands.run("drenv", "dump", "example.yaml")
+    dump = yaml.safe_load(out)
+    assert dump["profiles"][0]["name"] == "ex1"
+    assert dump["profiles"][1]["name"] == "ex2"
+
+
+def test_dump_with_prefix():
+    out = commands.run("drenv", "dump", "--name-prefix", "test-", "example.yaml")
+    dump = yaml.safe_load(out)
+    assert dump["profiles"][0]["name"] == "test-ex1"
+    assert dump["profiles"][1]["name"] == "test-ex2"
 
 
 def test_stop_unknown():
