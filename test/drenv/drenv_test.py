63ff67a3cb67e76d017f5732bc5d799ece5a6baf
Nir Soffer <nsoffer@redhat.com>
2023-04-25 18:32:28 +0300

    Add constants for file locations
    Preparing for adding sub directories, add module constants for various
    files. This will make it easier to maintain when we change the directory
    structure of the tree.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- test/drenv/drenv_test.py
+++ test/drenv/drenv_test.py
@@ -10,27 +10,30 @@ import pytest
 from drenv import cluster
 from drenv import commands
 
+EXAMPLE_ENV = "example.yaml"
+EXTERNAL_ENV = "external.yaml"
+
 
 def test_start_unknown():
     # Cluster does not exists, so it should fail.
     with pytest.raises(commands.Error):
-        commands.run("drenv", "start", "--name-prefix", "unknown-", "external.yaml")
+        commands.run("drenv", "start", "--name-prefix", "unknown-", EXTERNAL_ENV)
 
 
 def test_start(tmpenv):
-    commands.run("drenv", "start", "--name-prefix", tmpenv.prefix, "external.yaml")
+    commands.run("drenv", "start", "--name-prefix", tmpenv.prefix, EXTERNAL_ENV)
     assert cluster.status(tmpenv.prefix + "cluster") == cluster.READY
 
 
 def test_dump_without_prefix():
-    out = commands.run("drenv", "dump", "example.yaml")
+    out = commands.run("drenv", "dump", EXAMPLE_ENV)
     dump = yaml.safe_load(out)
     assert dump["profiles"][0]["name"] == "ex1"
     assert dump["profiles"][1]["name"] == "ex2"
 
 
 def test_dump_with_prefix():
-    out = commands.run("drenv", "dump", "--name-prefix", "test-", "example.yaml")
+    out = commands.run("drenv", "dump", "--name-prefix", "test-", EXAMPLE_ENV)
     dump = yaml.safe_load(out)
     assert dump["profiles"][0]["name"] == "test-ex1"
     assert dump["profiles"][1]["name"] == "test-ex2"
@@ -38,23 +41,23 @@ def test_dump_with_prefix():
 
 def test_stop_unknown():
     # Does nothing, so should succeed.
-    commands.run("drenv", "stop", "--name-prefix", "unknown-", "external.yaml")
+    commands.run("drenv", "stop", "--name-prefix", "unknown-", EXTERNAL_ENV)
 
 
 def test_stop(tmpenv):
     # Stop does nothing, so cluster must be ready.
-    commands.run("drenv", "stop", "--name-prefix", tmpenv.prefix, "external.yaml")
+    commands.run("drenv", "stop", "--name-prefix", tmpenv.prefix, EXTERNAL_ENV)
     assert cluster.status(tmpenv.prefix + "cluster") == cluster.READY
 
 
 def test_delete_unknown():
     # Does nothing, so should succeed.
-    commands.run("drenv", "delete", "--name-prefix", "unknown-", "external.yaml")
+    commands.run("drenv", "delete", "--name-prefix", "unknown-", EXTERNAL_ENV)
 
 
 def test_delete(tmpenv):
     # Delete does nothing, so cluster must be ready.
-    commands.run("drenv", "delete", "--name-prefix", tmpenv.prefix, "external.yaml")
+    commands.run("drenv", "delete", "--name-prefix", tmpenv.prefix, EXTERNAL_ENV)
     assert cluster.status(tmpenv.prefix + "cluster") == cluster.READY
 
 
