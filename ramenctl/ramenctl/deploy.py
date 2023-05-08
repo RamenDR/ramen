27b6a2b4d5d3585a88bed093e706327171bbb50d
Nir Soffer <nsoffer@redhat.com>
2023-05-08 23:12:01 +0300

    Implement 'ramenctl deploy' command
    Port `test/scripts/deploy-ramen` to a `ramenctl deploy` command, and add
    command lines options:
    
    - `--hub-name`: for working with clusters with non-default names
    - `--cluster-names`: for working with clusters with non-default names
    - `--image`: for installing custom image in your private repo or using a
      non-default tag.
    - `--source-dir`: for running in any directory
    
    The old script will be removed in a later commit.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- ramenctl/ramenctl/deploy.py
+++ ramenctl/ramenctl/deploy.py
@@ -1,6 +1,14 @@
 # SPDX-FileCopyrightText: The RamenDR authors
 # SPDX-License-Identifier: Apache-2.0
 
+import concurrent.futures
+import os
+import tempfile
+
+from . import command
+
+IMAGE = "quay.io/ramendr/ramen-operator:latest"
+
 
 def register(commands):
     parser = commands.add_parser(
@@ -8,7 +16,34 @@ def register(commands):
         help="Deploy ramen on the hub and managed clusters",
     )
     parser.set_defaults(func=run)
+    command.add_common_arguments(parser)
+    command.add_source_arguments(parser)
+    parser.add_argument(
+        "--image",
+        default=IMAGE,
+        help=f"The container image to deploy (default '{IMAGE}')",
+    )
 
 
 def run(args):
-    pass
+    with tempfile.TemporaryDirectory(prefix="ramenctl-deploy-") as tmpdir:
+        tar = os.path.join(tmpdir, "image.tar")
+        command.info("Saving image '%s'", args.image)
+        command.watch("podman", "save", args.image, "-o", tar)
+
+        def load_image(cluster):
+            command.info("Loading image in cluster '%s'", cluster)
+            command.watch("minikube", "--profile", cluster, "image", "load", tar)
+
+        clusters = [args.hub_name] + args.clusters_names
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            list(executor.map(load_image, clusters))
+
+    command.info("Deploying ramen operator in cluster '%s'", args.hub_name)
+    command.watch("kubectl", "config", "use-context", args.hub_name)
+    command.watch("make", "-C", args.source_dir, "deploy-hub")
+
+    for cluster in args.clusters_names:
+        command.info("Deploying ramen operator in cluster '%s'", cluster)
+        command.watch("kubectl", "config", "use-context", cluster)
+        command.watch("make", "-C", args.source_dir, "deploy-dr-cluster")
