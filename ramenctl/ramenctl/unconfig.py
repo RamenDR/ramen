7c0f4ce2a64cc71d9765facb3c2408f7f05c47ed
Nir Soffer <nsoffer@redhat.com>
2023-05-11 04:41:24 +0300

    Implement `ramenctl unconfig` command
    Port `test/ramen-config/undeploy` to `ramenctl unconfig` command, adding
    the same command line options used by the `ramenctl config` commands.
    
    The old script will be removed in later commit.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- ramenctl/ramenctl/unconfig.py
+++ ramenctl/ramenctl/unconfig.py
@@ -1,6 +1,10 @@
 # SPDX-FileCopyrightText: The RamenDR authors
 # SPDX-License-Identifier: Apache-2.0
 
+from drenv import kubectl
+
+from . import command
+
 
 def register(commands):
     parser = commands.add_parser(
@@ -8,7 +12,37 @@ def register(commands):
         help="Unconfigure ramen hub operator",
     )
     parser.set_defaults(func=run)
+    command.add_common_arguments(parser)
+    command.add_ramen_arguments(parser)
+    command.add_env_arguments(parser)
 
 
 def run(args):
-    pass
+    command.info("Deleting samples channel")
+    kubectl.delete(
+        "--filename",
+        command.resource("samples-channel.yaml"),
+        "--ignore-not-found",
+        context=args.hub_name,
+        log=command.debug,
+    )
+
+    command.info("Deleting DRClusters and DRPolicy")
+    kubectl.delete(
+        "--kustomize",
+        command.resource(args.env_type),
+        "--ignore-not-found",
+        context=args.hub_name,
+        log=command.debug,
+    )
+
+    # We keep the ramen config map since we do not own it.
+
+    command.info("Deleting s3 secret in ramen hub namespace")
+    kubectl.delete(
+        "--filename",
+        command.resource("s3-secret.yaml"),
+        "--ignore-not-found",
+        context=args.hub_name,
+        log=command.debug,
+    )
