dab285c9adbf0cb7516cbbda01a67f7627cb3af9
Elena Gershkovich <elenage@il.ibm.com>
2023-05-28 16:44:05 +0300

    Ensure required CRDs are part of the cluster before starting the reconciler
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>

--- controllers/volumereplicationgroup_controller.go
+++ controllers/volumereplicationgroup_controller.go
@@ -21,8 +21,10 @@ import (
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/api/meta"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
 	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -39,6 +41,10 @@ import (
 	ramendrv1alpha1 "github.com/ramendr/ramen/api/v1alpha1"
 	rmnutil "github.com/ramendr/ramen/controllers/util"
 	"github.com/ramendr/ramen/controllers/volsync"
+)
+
+const (
+	crdCheckInterval = 5 * time.Second
 )
 
 // VolumeReplicationGroupReconciler reconciles a VolumeReplicationGroup object
@@ -59,6 +65,10 @@ func (r *VolumeReplicationGroupReconciler) SetupWithManager(
 	r.eventRecorder = rmnutil.NewEventReporter(mgr.GetEventRecorderFor("controller_VolumeReplicationGroup"))
 
 	r.Log.Info("Adding VolumeReplicationGroup controller")
+
+	if err := r.waitForCrds(); err != nil {
+		return err
+	}
 
 	rateLimiter := workqueue.NewMaxOfRateLimiter(
 		workqueue.NewItemExponentialFailureRateLimiter(1*time.Second, 1*time.Minute),
@@ -144,6 +154,69 @@ func (r *VolumeReplicationGroupReconciler) configMapFun(configmap client.Object)
 	}
 
 	return req
+}
+
+func (r *VolumeReplicationGroupReconciler) waitForCrds() error {
+	requiredCrds := []string{"VolumeReplicationClass", "VolumeReplication"}
+
+	for _, crd := range requiredCrds {
+		err := r.waitForResource(crd)
+		if err != nil {
+			r.Log.Error(err, "unexpected error occurred while checking CRD", "CRD", crd)
+
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (r *VolumeReplicationGroupReconciler) waitForResource(resourceName string) error {
+	result, err := r.resourceExists(resourceName)
+	if err != nil {
+		return err
+	}
+
+	if !result {
+		r.Log.Info(fmt.Sprintf("CRD %s is not installed on cluster, waiting until it appears", resourceName))
+
+		for {
+			time.Sleep(crdCheckInterval)
+
+			result, err := r.resourceExists(resourceName)
+			if err != nil {
+				return err
+			}
+
+			if result {
+				r.Log.Info(fmt.Sprintf("CRD %s appeared on cluster", resourceName))
+
+				break
+			}
+		}
+	}
+
+	return nil
+}
+
+func (r *VolumeReplicationGroupReconciler) resourceExists(resourceName string) (bool, error) {
+	unstructuredResource := &unstructured.UnstructuredList{}
+	unstructuredResource.SetGroupVersionKind(schema.GroupVersionKind{
+		Group:   volrep.GroupVersion.Group,
+		Kind:    resourceName,
+		Version: volrep.GroupVersion.Version,
+	})
+
+	err := r.Client.List(context.TODO(), unstructuredResource)
+	if err != nil {
+		if !meta.IsNoMatchError(err) {
+			return false, err
+		}
+
+		return false, nil
+	}
+
+	return true, nil
 }
 
 func init() {
