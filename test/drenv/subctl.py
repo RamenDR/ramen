2b69d248fda72502ad55ba07104dd387d5f76790
Nir Soffer <nsoffer@redhat.com>
2023-06-12 23:26:33 +0300

    Add drenv subctl module
    Like other tools we depend on, we want a wrapper to make using the tool
    easier and less error prone.
    
    In this case the tool provide a missing feature, storing the
    broker-info.subm file in a different location.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ test/drenv/subctl.py
@@ -0,0 +1,41 @@
+# SPDX-FileCopyrightText: The RamenDR authors
+# SPDX-License-Identifier: Apache-2.0
+
+import shutil
+
+from . import commands
+
+BROKER_INFO = "broker-info.subm"
+
+
+def deploy_broker(context, globalnet=False, broker_info=None, log=print):
+    """
+    Run subctl deploy-broker ... logging progress messages.
+
+    If broker_info is not specified, a BROKER_INFO file is created in the
+    current directory. Otherwise the file is moved to the specified path.
+    """
+    args = ["deploy-broker", "--context", context]
+    if globalnet:
+        args.append("--globalnet")
+
+    _watch(*args, log=log)
+
+    if broker_info:
+        shutil.move(BROKER_INFO, broker_info)
+
+
+def join(broker_info, context, clusterid, cable_driver=None, log=print):
+    """
+    Run subctl join ... logging progress messages.
+    """
+    args = ["join", broker_info, "--context", context, "--clusterid", clusterid]
+    if cable_driver:
+        args.extend(("--cable-driver", cable_driver))
+    _watch(*args, log=log)
+
+
+def _watch(cmd, *args, context=None, log=print):
+    cmd = ["subctl", cmd, *args]
+    for line in commands.watch(*cmd):
+        log(line)
