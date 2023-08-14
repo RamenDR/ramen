cc864096d2998efedb63d09344dd8f9cd31f733f
rakeshgm <rakeshgm@redhat.com>
2023-08-15 02:30:11 +0530

    decouple updating DRPC Progression func()
    Signed-off-by: rakeshgm <rakeshgm@redhat.com>

--- controllers/drplacementcontrol.go
+++ controllers/drplacementcontrol.go
@@ -2055,13 +2055,23 @@ func (d *DRPCInstance) setDRState(nextState rmn.DRState) {
 	}
 }
 
-func (d *DRPCInstance) setProgression(nextProgression rmn.ProgressionStatus) {
-	if d.instance.Status.Progression != nextProgression {
-		d.log.Info(fmt.Sprintf("Progression: Current '%s'. Next '%s'",
-			d.instance.Status.Progression, nextProgression))
+func updateDRPCProgression(
+	drpc *rmn.DRPlacementControl, nextProgression rmn.ProgressionStatus, log logr.Logger,
+) bool {
+	if drpc.Status.Progression != nextProgression {
+		log.Info(fmt.Sprintf("Progression: Current '%s'. Next '%s'",
+			drpc.Status.Progression, nextProgression))
 
-		d.instance.Status.Progression = nextProgression
+		drpc.Status.Progression = nextProgression
+
+		return true
 	}
+
+	return false
+}
+
+func (d *DRPCInstance) setProgression(nextProgression rmn.ProgressionStatus) {
+	updateDRPCProgression(d.instance, nextProgression, d.log)
 }
 
 //nolint:cyclop
