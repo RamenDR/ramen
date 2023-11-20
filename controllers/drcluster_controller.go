e720ff5e542adef899191ec28ffa370d09aed7c2
Nir Soffer <nsoffer@redhat.com>
2023-11-21 00:26:35 +0200

    Add drClusterIsDeleted() helper
    In cluster replacement flow, we may need to disable DR when a drcluster
    is not available, which breaks disable DR flow in many places.
    
    To allow cleanup to proceed on the hub and on the remaining managed
    cluster, the user will have to delete the drcluster for the failed
    cluster.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- controllers/drcluster_controller.go
+++ controllers/drcluster_controller.go
@@ -294,7 +294,7 @@ func (r *DRClusterReconciler) Reconcile(ctx context.Context, req ctrl.Request) (
 
 	u.initializeStatus()
 
-	if !u.object.ObjectMeta.DeletionTimestamp.IsZero() {
+	if drClusterIsDeleted(drcluster) {
 		return r.processDeletion(u)
 	}
 
@@ -358,6 +358,11 @@ func (r DRClusterReconciler) processCreateOrUpdate(u *drclusterInstance) (ctrl.R
 	}
 
 	return ctrl.Result{Requeue: requeue || u.requeue}, reconcileError
+}
+
+// Return true if dr cluster was marked for deletion.
+func drClusterIsDeleted(c *ramen.DRCluster) bool {
+	return !c.GetDeletionTimestamp().IsZero()
 }
 
 func (u *drclusterInstance) initializeStatus() {
