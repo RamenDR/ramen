148fb35a98be59418a9aff8ca992943e38d6f4a4
Raghavendra Talur <raghavendra.talur@gmail.com>
2024-03-15 03:59:50 -0400

    tests: keep all suite cleanup functions in suite_test.go
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>
    (cherry picked from commit 5e1f1871b0c4a8ec1f1c47360479f32047f29882)

--- internal/controller/suite_test.go
+++ internal/controller/suite_test.go
@@ -242,6 +242,7 @@ var _ = BeforeSuite(func() {
 	ramenConfig.DrClusterOperator.S3SecretDistributionEnabled = true
 	ramenConfig.MultiNamespace.FeatureEnabled = true
 	configMapCreate(ramenConfig)
+	DeferCleanup(configMapDelete)
 
 	s3Secrets[0] = corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{Namespace: ramenNamespace, Name: "s3secret0"},
