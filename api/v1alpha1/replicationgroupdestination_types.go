5e9f488f3dd7277368e953e86cc680528d987d48
youhangwang <youhangwang@foxmail.com>
2024-05-20 16:44:56 +0800

    add rgs and rgd crd
    Signed-off-by: youhangwang <youhangwang@foxmail.com>

---
+++ api/v1alpha1/replicationgroupdestination_types.go
@@ -0,0 +1,72 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package v1alpha1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// ReplicationGroupDestinationSpec defines the desired state of ReplicationGroupDestination
+type ReplicationGroupDestinationSpec struct {
+	// Label selector to identify the VolumeSnapshotClass resources
+	// that are scanned to select an appropriate VolumeSnapshotClass
+	// for the VolumeReplication resource when using VolSync.
+	//+optional
+	VolumeSnapshotClassSelector metav1.LabelSelector `json:"volumeSnapshotClassSelector,omitempty"`
+
+	RDSpecs []VolSyncReplicationDestinationSpec `json:"rdSpecs,omitempty"`
+}
+
+// ReplicationGroupDestinationStatus defines the observed state of ReplicationGroupDestination
+type ReplicationDestinationStatus struct {
+	// lastSyncTime is the time of the most recent successful synchronization.
+	//+optional
+	LastSyncTime *metav1.Time `json:"lastSyncTime,omitempty"`
+	// lastSyncStartTime is the time the most recent synchronization started.
+	//+optional
+	LastSyncStartTime *metav1.Time `json:"lastSyncStartTime,omitempty"`
+	// lastSyncDuration is the amount of time required to send the most recent
+	// update.
+	//+optional
+	LastSyncDuration *metav1.Duration `json:"lastSyncDuration,omitempty"`
+	// nextSyncTime is the time when the next volume synchronization is
+	// scheduled to start (for schedule-based synchronization).
+	//+optional
+	NextSyncTime *metav1.Time `json:"nextSyncTime,omitempty"`
+
+	// latestImage in the object holding the most recent consistent replicated
+	// image.
+	//+optional
+	LatestImages []*corev1.TypedLocalObjectReference `json:"latestImage,omitempty"`
+
+	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	ReplicationDestinations []*corev1.ObjectReference `json:"replicationDestinations,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+//+kubebuilder:subresource:status
+
+// ReplicationGroupDestination is the Schema for the replicationgroupdestinations API
+type ReplicationGroupDestination struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ReplicationGroupDestinationSpec `json:"spec,omitempty"`
+	Status *ReplicationDestinationStatus   `json:"status,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// ReplicationGroupDestinationList contains a list of ReplicationGroupDestination
+type ReplicationGroupDestinationList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ReplicationGroupDestination `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ReplicationGroupDestination{}, &ReplicationGroupDestinationList{})
+}
--- go.mod
+++ go.mod
@@ -12,6 +12,7 @@ require (
 	github.com/go-logr/logr v1.3.0
 	github.com/google/uuid v1.3.1
 	github.com/kubernetes-csi/external-snapshotter/client/v4 v4.2.0
+	github.com/kubernetes-csi/external-snapshotter/client/v7 v7.0.0
 	github.com/onsi/ginkgo/v2 v2.13.0
 	github.com/onsi/gomega v1.30.0
 	github.com/open-cluster-management-io/api v0.0.0-00010101000000-000000000000
@@ -71,15 +72,16 @@ require (
 	github.com/prometheus/client_model v0.4.0 // indirect
 	github.com/prometheus/common v0.44.0 // indirect
 	github.com/prometheus/procfs v0.10.1 // indirect
+	github.com/robfig/cron/v3 v3.0.1 // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/net v0.25.0 // indirect
 	golang.org/x/oauth2 v0.11.0 // indirect
-	golang.org/x/sys v0.14.0 // indirect
-	golang.org/x/term v0.14.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.12.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/term v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	golang.org/x/tools v0.21.0 // indirect
 	gomodules.xyz/jsonpatch/v2 v2.4.0 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20231106174013-bbf56f31fb17 // indirect
