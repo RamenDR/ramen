46876a77740edb2313f27f7a0b0e4b8ad22f4a16
youhangwang <youhangwang@foxmail.com>
2024-05-20 16:44:56 +0800

    add rgs and rgd crd
    Signed-off-by: youhangwang <youhangwang@foxmail.com>

--- main.go
+++ main.go
@@ -234,6 +234,21 @@ func main() {
 
 	setupReconcilers(mgr, ramenConfig)
 
+	if err = (&controllers.ReplicationGroupDestinationReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ReplicationGroupDestination")
+		os.Exit(1)
+	}
+
+	if err = (&controllers.ReplicationGroupSourceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "ReplicationGroupSource")
+		os.Exit(1)
+	}
 	// +kubebuilder:scaffold:builder
 	if err := mgr.AddHealthzCheck("health", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
