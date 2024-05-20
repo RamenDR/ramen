3ef2de5460255f4eec625bcc7f358ce28734c326
jacklu <jilu@redhat.com>
2024-05-20 16:34:19 +0800

    use one single config.yaml for all inputs
    Signed-off-by: jacklu <jilu@redhat.com>

--- e2e/util/config.go
+++ e2e/util/config.go
@@ -6,6 +6,7 @@ package util
 import (
 	"fmt"
 
+	"github.com/go-logr/logr"
 	"github.com/spf13/viper"
 )
 
@@ -13,11 +14,14 @@ type TestConfig struct {
 	ChannelName      string
 	ChannelNamespace string
 	GitURL           string
+	Clusters         map[string]struct {
+		KubeconfigPath string `mapstructure:"kubeconfigpath" required:"true"`
+	} `mapstructure:"clusters" required:"true"`
 }
 
 var config = &TestConfig{}
 
-func ReadConfig() error {
+func ReadConfig(log *logr.Logger, configFile string) error {
 	viper.SetDefault("ChannelName", defaultChannelName)
 	viper.SetDefault("ChannelNamespace", defaultChannelNamespace)
 	viper.SetDefault("GitURL", defaultGitURL)
@@ -34,7 +38,13 @@ func ReadConfig() error {
 		return (err)
 	}
 
-	viper.SetConfigFile("config.yaml")
+	if configFile == "" {
+		log.Info("No configuration file specified, using default value config.yaml")
+
+		configFile = "config.yaml"
+	}
+
+	viper.SetConfigFile(configFile)
 
 	if err := viper.ReadInConfig(); err != nil {
 		return fmt.Errorf("failed to read config: %v", err)
@@ -42,6 +52,18 @@ func ReadConfig() error {
 
 	if err := viper.Unmarshal(config); err != nil {
 		return fmt.Errorf("failed to unmarshal config: %v", err)
+	}
+
+	if config.Clusters["hub"].KubeconfigPath == "" {
+		return fmt.Errorf("failed to find hub cluster in configuration")
+	}
+
+	if config.Clusters["c1"].KubeconfigPath == "" {
+		return fmt.Errorf("failed to find c1 cluster in configuration")
+	}
+
+	if config.Clusters["c2"].KubeconfigPath == "" {
+		return fmt.Errorf("failed to find c2 cluster in configuration")
 	}
 
 	return nil
