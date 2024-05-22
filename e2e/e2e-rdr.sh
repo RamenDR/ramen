5c7e45c226d576095685d43672cbe64b2999ca4a
Nir Soffer <nsoffer@redhat.com>
2024-05-22 15:05:58 +0300

    Fail e2e workflow if tests failed
    Now that that we have basic test running, we want to fail the workflow
    if the tests failed. Without this  people assumes code changes are
    passed the tests.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- e2e/e2e-rdr.sh
+++ e2e/e2e-rdr.sh
@@ -6,5 +6,3 @@
 echo "Running tests..."
 
 go test -kubeconfig-c1 ~/.config/drenv/rdr-rdr/kubeconfigs/rdr-dr1  -kubeconfig-c2 ~/.config/drenv/rdr-rdr/kubeconfigs/rdr-dr2 -kubeconfig-hub ~/.config/drenv/rdr-rdr/kubeconfigs/rdr-hub -timeout 0 -v "$@"
-
-exit 0
