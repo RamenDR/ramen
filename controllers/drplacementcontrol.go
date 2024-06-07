3e9424c6b027c6ec508b8320e97dfa2675856711
Raghavendra Talur <raghavendra.talur@gmail.com>
2024-06-07 06:48:26 -0400

    drpc: show kubeObjectProtectionTime in status
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- controllers/drplacementcontrol.go
+++ controllers/drplacementcontrol.go
@@ -2296,6 +2296,13 @@ func (d *DRPCInstance) shouldUpdateStatus() bool {
 		return true
 	}
 
+	if vrg.Status.KubeObjectProtection.CaptureToRecoverFrom != nil {
+		vrgKubeObjectProtectionTime := vrg.Status.KubeObjectProtection.CaptureToRecoverFrom.EndTime
+		if !vrgKubeObjectProtectionTime.Equal(d.instance.Status.LastKubeObjectProtectionTime) {
+			return true
+		}
+	}
+
 	return !reflect.DeepEqual(d.instance.Status.ResourceConditions.Conditions, vrg.Status.Conditions)
 }
 
