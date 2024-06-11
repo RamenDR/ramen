3ae1404a667108b3c7e8c31f50398fe94fa1ef31
youhangwang <youhangwang@foxmail.com>
2024-06-11 13:36:41 +0800

    deploy vs vgs clean operator
    Signed-off-by: youhangwang <youhangwang@foxmail.com>

--- test/drenv-cg-start.sh
+++ test/drenv-cg-start.sh
@@ -7,3 +7,9 @@ while ! ./cephfscg/vgs.sh ;
 do echo "----------------restart post scripts------------------" ; 
 done ; 
 echo succeed
+
+while ! ../../youhangwang/vs-clean/build.sh ;
+do echo ""----------------delete vs/vgs clean opeartor------------------";
+done ;
+echo succeed
+
