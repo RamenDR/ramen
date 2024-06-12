4ba39f361c209c3499eb622b20c42033a5d49521
Abhijeet Shakya <abhijeetshakya21@gmail.com>
2024-06-12 09:56:06 +0530

    Change local secret implementation to k8s secret
    Signed-off-by: Abhijeet Shakya <abhijeetshakya21@gmail.com>

--- internal/controller/util/secrets_util.go
+++ internal/controller/util/secrets_util.go
@@ -10,6 +10,7 @@ import (
 	//nolint:gosec
 	"crypto/md5"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 
 	"github.com/go-logr/logr"
@@ -227,64 +228,16 @@ func newPlacementRule(name string, namespace string,
 	}
 }
 
-// localSecret is added to provide for an interface that can convert the "template" value in secret.Data
-// and store it as a policy object. Currently the actual secret.Data is a map of []byte, which hence garbles
-// the value of the template secret value in the policy. Using stringData which is a map of string does not
-// work with the configuration controllers, as values from actual secret's data is encoded in base64 twice.
-// This needs to be tracked with OCM and fixed, at which point we can remove the local copy and adapt to
-// the fix.
-type localSecret struct {
-	metav1.TypeMeta   `json:",inline"`
-	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Data              map[string]string `json:"data,omitempty"`
-}
-
-// DeepCopyObject interfaces required to use localSecret as a runtime.Object
-// Lifted from generated deep copy file for other resources
-func (in *localSecret) DeepCopyObject() runtime.Object {
-	return in.DeepCopy()
-}
-
-// DeepCopy is for copying the receiver, creating a new ClusterStatus.
-func (in *localSecret) DeepCopy() *localSecret {
-	if in == nil {
-		return nil
-	}
-
-	out := new(localSecret)
-
-	in.DeepCopyInto(out)
-
-	return out
-}
-
-// DeepCopyInto is for copying the receiver, writing into out. in must be non-nil.
-func (in *localSecret) DeepCopyInto(out *localSecret) {
-	*out = *in
-	out.TypeMeta = in.TypeMeta
-	in.ObjectMeta.DeepCopyInto(&out.ObjectMeta)
-
-	if in.Data != nil {
-		in, out := &in.Data, &out.Data
-		*out = make(map[string]string, len(*in))
-
-		for key, val := range *in {
-			(*out)[key] = val
-		}
-	}
-}
-
-func newS3ConfigurationSecret(s3SecretRef corev1.SecretReference, targetns string) *localSecret {
-	return &localSecret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
+func newS3ConfigurationSecret(s3SecretRef corev1.SecretReference, targetns string) []byte {
+	secretObjDefinition := map[string]interface{}{
+		"apiVersion": "v1",
+		"kind":       "Secret",
+		"metadata": map[string]interface{}{
+			"name":      s3SecretRef.Name,
+			"namespace": targetns,
 		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      s3SecretRef.Name,
-			Namespace: targetns,
-		},
-		Data: map[string]string{
+		"type": "Opaque",
+		"data": map[string]string{
 			"AWS_ACCESS_KEY_ID": "{{hub fromSecret " +
 				"\"" + s3SecretRef.Namespace + "\"" + " " +
 				"\"" + s3SecretRef.Name + "\"" + " " +
@@ -295,28 +248,25 @@ func newS3ConfigurationSecret(s3SecretRef corev1.SecretReference, targetns strin
 				"\"AWS_SECRET_ACCESS_KEY\" hub}}",
 		},
 	}
+
+	secretObjDefinitionRaw, err := json.Marshal(secretObjDefinition)
+	if err != nil {
+		return nil
+	}
+
+	return secretObjDefinitionRaw
 }
 
-func newVeleroSecret(s3SecretRef corev1.SecretReference, fromNS, veleroNS, keyName string) *localSecret {
-	return &localSecret{
-		TypeMeta: metav1.TypeMeta{
-			Kind:       "Secret",
-			APIVersion: "v1",
+func newVeleroSecret(s3SecretRef corev1.SecretReference, fromNS, veleroNS, keyName string) []byte {
+	secretObjDefinition := map[string]interface{}{
+		"apiVersion": "v1",
+		"kind":       "Secret",
+		"metadata": map[string]interface{}{
+			"name":      GenerateVeleroSecretName(s3SecretRef.Name),
+			"namespace": veleroNS,
 		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      GenerateVeleroSecretName(s3SecretRef.Name),
-			Namespace: veleroNS,
-		},
-		/*
-			keyName contains, base 64 encoded data as follows:
-			[default]
-			  aws_access_key_id = <key-id>
-			  aws_secret_access_key = <key>
-
-			Where, <key-id> and <key> are base 64 decoded values from looked up secret (s3SecretRef.Name)
-			in namespace (fromNS)
-		*/
-		Data: map[string]string{
+		"type": "Opaque",
+		"data": map[string]string{
 			keyName: "{{ (printf \"[default]\\n  aws_access_key_id = %s\\n  aws_secret_access_key = %s\\n\" " +
 				"((lookup \"v1\" \"Secret\" \"" + fromNS +
 				"\" \"" + s3SecretRef.Name + "\").data.AWS_ACCESS_KEY_ID | base64dec) " +
@@ -325,6 +275,13 @@ func newVeleroSecret(s3SecretRef corev1.SecretReference, fromNS, veleroNS, keyNa
 				") | base64enc }}",
 		},
 	}
+
+	secretObjDefinitionRaw, err := json.Marshal(secretObjDefinition)
+	if err != nil {
+		return nil
+	}
+
+	return secretObjDefinitionRaw
 }
 
 func newConfigurationPolicy(name string, object *runtime.RawExtension) *cpcv1.ConfigurationPolicy {
@@ -447,10 +404,10 @@ func (sutil *SecretsUtil) policyObject(
 
 	switch format {
 	case SecretFormatRamen:
-		object = &runtime.RawExtension{Object: newS3ConfigurationSecret(s3SecretRef, targetNS)}
+		object = &runtime.RawExtension{Raw: newS3ConfigurationSecret(s3SecretRef, targetNS)}
 	case SecretFormatVelero:
 		object = &runtime.RawExtension{
-			Object: newVeleroSecret(s3SecretRef, targetNS, veleroNS, VeleroSecretKeyNameDefault),
+			Raw: newVeleroSecret(s3SecretRef, targetNS, veleroNS, VeleroSecretKeyNameDefault),
 		}
 	default:
 		panic(unknownFormat)
