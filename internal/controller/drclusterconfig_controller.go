7749558ea72ad30732f5cbd7382f2da00fcbc0df
Shyamsundar Ranganathan <srangana@redhat.com>
2024-07-12 09:49:11 -0400

    Add logger to DRClusterConfig reconciler
    Also, cleanup some scaffolding comments.
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- internal/controller/drclusterconfig_controller.go
+++ internal/controller/drclusterconfig_controller.go
@@ -11,6 +11,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
+	"github.com/go-logr/logr"
+	"github.com/google/uuid"
 	ramendrv1alpha1 "github.com/ramendr/ramen/api/v1alpha1"
 )
 
@@ -18,25 +20,21 @@ import (
 type DRClusterConfigReconciler struct {
 	client.Client
 	Scheme *runtime.Scheme
+	Log    logr.Logger
 }
 
-//+kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs/finalizers,verbs=update
+//nolint:lll
+// +kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=ramendr.openshift.io,resources=drclusterconfigs/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the DRClusterConfig object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
 func (r *DRClusterConfigReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	_ = log.FromContext(ctx)
 
-	// TODO(user): your logic here
+	log := r.Log.WithValues("name", req.NamespacedName.Name, "rid", uuid.New())
+	log.Info("reconcile enter")
+
+	defer log.Info("reconcile exit")
 
 	return ctrl.Result{}, nil
 }
