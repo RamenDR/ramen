5ea67ceb936ad0f028126499cff4f053436907cb
Shyamsundar Ranganathan <srangana@redhat.com>
2024-07-12 09:52:32 -0400

    Add StorageClass listing and dummy functions for claim creation
    Building the scaffold for the overall functionality.
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- internal/controller/drclusterconfig_controller.go
+++ internal/controller/drclusterconfig_controller.go
@@ -9,6 +9,7 @@ import (
 	"time"
 
 	"golang.org/x/time/rate"
+	storagev1 "k8s.io/api/storage/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/client-go/util/workqueue"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -25,6 +26,9 @@ const (
 	drCConfigFinalizerName = "drclusterconfigs.ramendr.openshift.io/ramen"
 
 	maxReconcileBackoff = 5 * time.Minute
+
+	// Prefixes for various ClusterClaims
+	ccSCPrefix = "storage.class"
 )
 
 // DRClusterConfigReconciler reconciles a DRClusterConfig object
@@ -62,22 +66,35 @@ func (r *DRClusterConfigReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 	return r.processCreateOrUpdate(ctx, log, drCConfig)
 }
 
+// processDeletion ensures all cluster claims created by drClusterConfig are deleted, before removing the finalizer on
+// the resource itself
 func (r *DRClusterConfigReconciler) processDeletion(
 	ctx context.Context,
 	log logr.Logger,
 	drCConfig *ramen.DRClusterConfig,
 ) (ctrl.Result, error) {
+	if err := r.pruneClusterClaims(ctx, log, []string{}); err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+
 	if err := util.NewResourceUpdater(drCConfig).
 		RemoveFinalizer(drCConfigFinalizerName).
 		Update(ctx, r.Client); err != nil {
-		log.Info("Failed to remove finalizer for DRClusterConfig resource", "error", err)
-
-		return ctrl.Result{Requeue: true}, nil
+		return ctrl.Result{Requeue: true},
+			fmt.Errorf("failed to remove finalizer for DRClusterConfig resource, %w", err)
 	}
 
 	return ctrl.Result{}, nil
 }
 
+// pruneClusterClaims will prune all ClusterClaims created by drClusterConfig that are not in the
+// passed in survivor list
+func (r *DRClusterConfigReconciler) pruneClusterClaims(ctx context.Context, log logr.Logger, survivors []string) error {
+	return nil
+}
+
+// processCreateOrUpdate protects the resource with a finalizer and creates ClusterClaims for various storage related
+// classes in the cluster. It would finally prune stale ClusterClaims from previous reconciliations.
 func (r *DRClusterConfigReconciler) processCreateOrUpdate(
 	ctx context.Context,
 	log logr.Logger,
@@ -86,12 +103,65 @@ func (r *DRClusterConfigReconciler) processCreateOrUpdate(
 	if err := util.NewResourceUpdater(drCConfig).
 		AddFinalizer(drCConfigFinalizerName).
 		Update(ctx, r.Client); err != nil {
-		log.Info("Failed to add finalizer for DRClusterConfig resource", "error", err)
+		return ctrl.Result{Requeue: true}, fmt.Errorf("failed to add finalizer for DRClusterConfig resource, %w", err)
+	}
 
-		return ctrl.Result{Requeue: true}, nil
+	allSurvivors := []string{}
+
+	survivors, err := r.createSCClusterClaims(ctx, log)
+	if err != nil {
+		return ctrl.Result{Requeue: true}, err
+	}
+
+	allSurvivors = append(allSurvivors, survivors...)
+
+	if err := r.pruneClusterClaims(ctx, log, allSurvivors); err != nil {
+		return ctrl.Result{Requeue: true}, err
 	}
 
 	return ctrl.Result{}, nil
+}
+
+// createSCClusterClaims lists all StorageClasses and creates ClusterClaims for them
+func (r *DRClusterConfigReconciler) createSCClusterClaims(
+	ctx context.Context,
+	log logr.Logger,
+) ([]string, error) {
+	claims := []string{}
+
+	sClasses := &storagev1.StorageClassList{}
+	if err := r.Client.List(ctx, sClasses); err != nil {
+		return nil, fmt.Errorf("failed to list StorageClasses, %w", err)
+	}
+
+	for i := range sClasses.Items {
+		// TODO: If something is labeled later is there an Update event?
+		if !util.HasLabel(&sClasses.Items[i], StorageIDLabel) {
+			continue
+		}
+
+		if err := r.ensureClusterClaim(ctx, log, ccSCPrefix, sClasses.Items[i].GetName()); err != nil {
+			return nil, err
+		}
+
+		claims = append(claims, claimName(ccSCPrefix, sClasses.Items[i].GetName()))
+	}
+
+	return claims, nil
+}
+
+// ensureClusterClaim is a generic ClusterClaim creation function, that create a claim named "prefix.name", with
+// the passed in name as the ClusterClaim spec.Value
+func (r *DRClusterConfigReconciler) ensureClusterClaim(
+	ctx context.Context,
+	log logr.Logger,
+	prefix, name string,
+) error {
+	return nil
+}
+
+func claimName(prefix, name string) string {
+	return prefix + "." + name
 }
 
 // SetupWithManager sets up the controller with the Manager.
