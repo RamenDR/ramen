e23268a0f1a2edd55ba9c9e7eaa0f5e701c2c787
ShyamsundarR <srangana@redhat.com>
2024-09-19 05:56:10 -0400

    Ensure deleted DRPolicies do not add schedules to DRClusterConfig (#1565)
    * Ensure deleted DRPolicies do not add schedules to DRClusterConfig
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>
    
    * Correct external snapshotter dependency to 0.8
    
    Was introduced as part of the commit 91e5a5ba
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>
    
    * Fetch external-snapshotter resources using raw URL
    
    Older scheme was using a git clone URL, this is more efficient as
    it fetches required resources direcly and avoids the clone.
    
    Ref.: https://github.com/kubernetes-sigs/kustomize/blob/master/examples/remoteBuild.md#remote-directories
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>
    
    ---------
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- internal/controller/drclusterconfig_controller.go
+++ internal/controller/drclusterconfig_controller.go
@@ -10,7 +10,7 @@ import (
 	"time"
 
 	volrep "github.com/csi-addons/kubernetes-csi-addons/api/replication.storage/v1alpha1"
-	snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v7/apis/volumesnapshot/v1"
+	snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumesnapshot/v1"
 	"golang.org/x/time/rate"
 	storagev1 "k8s.io/api/storage/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
