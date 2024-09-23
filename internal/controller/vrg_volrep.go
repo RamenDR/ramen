fcdddfd7e2989e58c60cc88d8e887bc32b5d3113
Nir Soffer <nsoffer@redhat.com>
2024-09-23 14:03:12 +0300

    Reformat reconcileMissingVR comment for readability
    The comment is still unclear, but at least easier to read.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- internal/controller/vrg_volrep.go
+++ internal/controller/vrg_volrep.go
@@ -886,14 +886,17 @@ func (v *VRGInstance) undoPVCFinalizersAndPVRetention(pvc *corev1.PersistentVolu
 
 // reconcileMissingVR determines if VR is missing, and if missing completes other steps required for
 // reconciliation during deletion.
-// VR can be missing,
-// - if no VR was created post initial processing, by when VRG was deleted. In this case
-// no PV was also uploaded, as VR is created first before PV is uploaded.
-// - if VR was deleted in a prior reconcile, during VRG deletion, but steps post VR deletion were not
-// completed, at this point a deleted VR is also not processed further (its generation would have been updated)
-// Returns 2 booleans,
-// - the first indicating if VR is missing or not, to enable further VR processing if needed
-// - the next indicating any required requeue of the request, due to errors in determining VR presence
+//
+// VR can be missing:
+//   - if no VR was created post initial processing, by when VRG was deleted. In this case no PV was also
+//     uploaded, as VR is created first before PV is uploaded.
+//   - if VR was deleted in a prior reconcile, during VRG deletion, but steps post VR deletion were not
+//     completed, at this point a deleted VR is also not processed further (its generation would have been
+//     updated)
+//
+// Returns 2 booleans:
+//   - the first indicating if VR is missing or not, to enable further VR processing if needed
+//   - the next indicating any required requeue of the request, due to errors in determining VR presence
 func (v *VRGInstance) reconcileMissingVR(pvc *corev1.PersistentVolumeClaim, log logr.Logger) (bool, bool) {
 	const (
 		requeue   = true
