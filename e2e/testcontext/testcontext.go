b504433c9640a8b1add42d0bbce467adf6d52ec5
Nir Soffer <nsoffer@redhat.com>
2024-10-22 18:37:40 +0300

    e2e: Simplify test context management
    We had 2 places using a test context - test suites, and action helpers.
    To work with both, we store the test context in a global map and had
    complicated search mechanism to locate the context in both the parent
    and child sub-tests.
    
    Simplify the design by moving the actions wrappers (deploy, enable, ..)
    to the test context, and passing a text context to the function running
    the test flow.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- e2e/testcontext/testcontext.go
+++ e2e/testcontext/testcontext.go
@@ -4,11 +4,10 @@
 package testcontext
 
 import (
-	"fmt"
-	"strings"
-	"sync"
+	"testing"
 
 	"github.com/ramendr/ramen/e2e/deployers"
+	"github.com/ramendr/ramen/e2e/dractions"
 	"github.com/ramendr/ramen/e2e/workloads"
 )
 
@@ -17,46 +16,50 @@ type TestContext struct {
 	Deployer deployers.Deployer
 }
 
-var mutex sync.Mutex
+func (c *TestContext) Deploy(t *testing.T) {
+	t.Helper()
 
-var contexts = make(map[string]TestContext)
-
-// Based on name passed, Init the deployer and Workload and stash in a map[string]TestContext
-func AddTestContext(name string, w workloads.Workload, d deployers.Deployer) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	contexts[name] = TestContext{w, d}
-}
-
-func DeleteTestContext(name string) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	delete(contexts, name)
-}
-
-// Search name in map for a TestContext to return, if not found go backward
-// - drop the last /<name> suffix form name and search
-// - e.g If name passed is "TestSuites/Exhaustive/DaemonSet/Subscription/Undeploy"
-//   - Search for above name first (it will not be found as we create context at a point where we have a d+w)
-//   - Search for "TestSuites/Exhaustive/DaemonSet/Subscription" (should be found)
-func GetTestContext(name string) (TestContext, error) {
-	mutex.Lock()
-	defer mutex.Unlock()
-
-	testCtx, ok := contexts[name]
-	if !ok {
-		i := strings.LastIndex(name, "/")
-		if i < 1 {
-			return TestContext{}, fmt.Errorf("not a valid name in TestContext: %v", name)
-		}
-
-		testCtx, ok = contexts[name[0:i]]
-		if !ok {
-			return TestContext{}, fmt.Errorf("can not find testContext with name: %v", name)
-		}
+	if err := c.Deployer.Deploy(c.Workload); err != nil {
+		t.Fatal(err)
 	}
+}
 
-	return testCtx, nil
+func (c *TestContext) Undeploy(t *testing.T) {
+	t.Helper()
+
+	if err := c.Deployer.Undeploy(c.Workload); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (c *TestContext) Enable(t *testing.T) {
+	t.Helper()
+
+	if err := dractions.EnableProtection(c.Workload, c.Deployer); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (c *TestContext) Disable(t *testing.T) {
+	t.Helper()
+
+	if err := dractions.DisableProtection(c.Workload, c.Deployer); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (c *TestContext) Failover(t *testing.T) {
+	t.Helper()
+
+	if err := dractions.Failover(c.Workload, c.Deployer); err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (c *TestContext) Relocate(t *testing.T) {
+	t.Helper()
+
+	if err := dractions.Relocate(c.Workload, c.Deployer); err != nil {
+		t.Fatal(err)
+	}
 }
