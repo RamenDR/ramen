268b5ff741f575ae862563e6935d89bec86fba0a
Shyamsundar Ranganathan <srangana@redhat.com>
2024-10-23 17:31:30 -0400

    Add functionality to getClusterClasses in peerClass update feature
    This commit adds:
    - Inspecting each DRCluster in the DRPolicy for related ManagedCluster
    claims for various storage related classes
    - Creating an MCV for such claims to fetch the corresponding class
    - Updating classLists with the information fetched per cluster
    - Pruning MCVs for classes that are no longer claimed
    - Deleting MCVs when DRCluster is deleted
    - Adding a watch for ManagedCluster and MCV in the DRPolicy reconciler
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- internal/controller/util/mw_util.go
+++ internal/controller/util/mw_util.go
@@ -45,6 +45,9 @@ const (
 	MWTypeNS        string = "ns"
 	MWTypeNF        string = "nf"
 	MWTypeMMode     string = "mmode"
+	MWTypeSClass    string = "sc"
+	MWTypeVSClass   string = "vsc"
+	MWTypeVRClass   string = "vrc"
 	MWTypeDRCConfig string = "drcconfig"
 )
 
