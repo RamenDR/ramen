6994cfa62f5c0647181cffd9398cfc20a294c47f
Abhijeet Shakya <abhijeetshakya21@gmail.com>
2024-10-30 19:36:34 +0530

    Updating unit-tests for drpolicy changes
    Signed-off-by: Abhijeet Shakya <abhijeetshakya21@gmail.com>

--- internal/controller/controllers_utils_test.go
+++ internal/controller/controllers_utils_test.go
@@ -270,7 +270,7 @@ func drclusterConditionExpect(
 }
 
 func validateClusterManifest(apiReader client.Reader, drcluster *ramen.DRCluster, disabled bool) {
-	expectedCount := 8
+	expectedCount := 2
 	if disabled {
 		expectedCount = 0
 	}
