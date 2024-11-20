59549dbda196a5ccb250054264b845bd2f557ec6
Nir Soffer <nsoffer@redhat.com>
2024-11-20 22:44:27 +0200

    Delete the legacy test environment
    The old scripts are not maintained for a while and were replaced by
    drenv and e2e. If someone want to use the scripts they can check out an
    older version.
    
    Fixes: #1657
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- hack/recipe_e2e/scripts/deploy_primary.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-kubectl apply -f config/ramen_secret_minio.yaml 
-
-kubectl apply -f config/ramen_config.yaml
-
-kubectl apply -f protect/vrg_busybox_primary.yaml
-
-kubectl apply -f protect/recipe_busybox.yaml
--- hack/velero-uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# SPDX-FileCopyrightText: The RamenDR authors
-# SPDX-License-Identifier: Apache-2.0
-
-# shellcheck disable=2086
-set -x
-set -e
-rm -f $1/velero
