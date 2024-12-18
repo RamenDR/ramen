0b89d361e2160f1c4b236af972840698ed4e4684
Benamar Mekhissi <bmekhiss@ibm.com>
2024-12-18 02:24:56 -0500

    Support original accessModes for non-CephFS source PVCs
    For non-CephFS PVCs, temporary PVCs now inherit the accessModes of the
    source PVC rather than using ReadOnlyMany.
    
    Signed-off-by: Benamar Mekhissi <bmekhiss@ibm.com>

--- internal/controller/cephfscg/utils.go
+++ internal/controller/cephfscg/utils.go
@@ -11,10 +11,8 @@ import (
 	"github.com/ramendr/ramen/internal/controller/volsync"
 	corev1 "k8s.io/api/core/v1"
 	storagev1 "k8s.io/api/storage/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	ctrlutil "sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // ------------- [Begin] Copied from existing code in Ramen ----
@@ -52,55 +50,6 @@ func getLocalServiceNameForRDFromPVCName(pvcName string) string {
 func getLocalServiceNameForRD(rdName string) string {
 	// This is the name VolSync will use for the service
 	return fmt.Sprintf("volsync-rsync-tls-dst-%s", rdName)
-}
-
-// ------------- [End] Copied from existing code in Ramen ----
-
-// ------------- [Begin] Edited from existing code in Ramen ----
-
-// Copied from func (v *VSHandler) ModifyRSSpecForCephFS
-func GetRestoreStorageClass(
-	ctx context.Context, k8sClient client.Client, storageClassName string,
-	defaultCephFSCSIDriverName string,
-) (*storagev1.StorageClass, error) {
-	storageClass, err := GetStorageClass(ctx, k8sClient, &storageClassName)
-	if err != nil {
-		return nil, err
-	}
-
-	if storageClass.Provisioner != defaultCephFSCSIDriverName {
-		return storageClass, nil // No workaround required
-	}
-
-	// Create/update readOnlyPVCStorageClass
-	readOnlyPVCStorageClass := &storagev1.StorageClass{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: storageClass.GetName() + "-vrg",
-		},
-	}
-
-	_, err = ctrlutil.CreateOrUpdate(ctx, k8sClient, readOnlyPVCStorageClass, func() error {
-		// Do not update the storageclass if it already exists - Provisioner and Parameters are immutable anyway
-		if readOnlyPVCStorageClass.CreationTimestamp.IsZero() {
-			readOnlyPVCStorageClass.Provisioner = storageClass.Provisioner
-
-			// Copy other parameters from the original storage class
-			readOnlyPVCStorageClass.Parameters = map[string]string{}
-			for k, v := range storageClass.Parameters {
-				readOnlyPVCStorageClass.Parameters[k] = v
-			}
-
-			// Set backingSnapshot parameter to true
-			readOnlyPVCStorageClass.Parameters["backingSnapshot"] = "true"
-		}
-
-		return nil
-	})
-	if err != nil {
-		return nil, fmt.Errorf("%w", err)
-	}
-
-	return readOnlyPVCStorageClass, nil
 }
 
 // Copied from func (v *VSHandler) getStorageClass(
