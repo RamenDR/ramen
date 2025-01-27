638c1528e080e3bc0e792ccba4df9141a99d8b27
Nir Soffer <nsoffer@redhat.com>
2025-01-27 23:31:40 +0200

    Log same upload error once
    If adding archived annotation to PVC or PV failed, we logged an error
    with a stacktrace immediately, and then we creating a new error wrapping
    the original error and passed it back to the caller. Then we logged the
    error using INFO level log and wrap the error again multiple times.
    
    The result is logging the same error multiple times with different text
    (since add test in each level). This makes it very hard to find the
    actual error and ignore the duplicates.
    
    Fix error handling for this code path so we log the same error exactly
    once, at the top level reconcile function.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

--- internal/controller/vrg_volrep.go
+++ internal/controller/vrg_volrep.go
@@ -105,8 +105,7 @@ func (v *VRGInstance) reconcileVolRepsAsPrimary() {
 		// subscription, such as, the deployment, pods, services, etc., by an
 		// entity external to the VRG a la IaC.
 		if err := v.uploadPVandPVCtoS3Stores(pvc, log); err != nil {
-			log.Info("Requeuing due to failure to upload PV object to S3 store(s)",
-				"errorValue", err)
+			log.Error(err, "Requeuing due to failure to upload PV object to S3 store(s)")
 
 			v.requeue()
 
@@ -645,13 +644,11 @@ func (v *VRGInstance) uploadPVandPVCtoS3Stores(pvc *corev1.PersistentVolumeClaim
 	}
 
 	if err := v.addArchivedAnnotationForPVC(pvc, log); err != nil {
-		msg := fmt.Sprintf("failed to add archived annotation for PVC (%s/%s) with error (%v)",
-			pvc.Namespace, pvc.Name, err)
-		v.log.Info(msg)
+		msg := fmt.Sprintf("failed to add archived annotation: %s", err)
 		v.updatePVCClusterDataProtectedCondition(pvc.Namespace, pvc.Name,
 			VRGConditionReasonClusterDataAnnotationFailed, msg)
 
-		return errors.New(msg)
+		return fmt.Errorf("failed to add archived annotation for PVC (%s/%s): %w", pvc.Namespace, pvc.Name, err)
 	}
 
 	msg := fmt.Sprintf("Done uploading PV/PVC cluster data to %d of %d S3 profile(s): %v",
