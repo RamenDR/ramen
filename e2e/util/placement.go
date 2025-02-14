3cfc11f8947de25f2492f4aeb6b2376f648eb7b6
Parikshith <pbyregow@redhat.com>
2025-02-14 09:03:01 -0500

    e2e: refactor cluster retrieval functions to util/placement.go
    - Moved functions responsible for retrieving the current & target clusters from dractions/retry.go to util/placement.go
    - Moved getPlacement from dractions/crug.go to util/placement.go
    - Refactored getTargetCluster to GetCurrentAndTargetCluster by merging logic to retrieve both current and target clusters into a single function.
    - Made GetCurrentCluster, GetCurrentAndTargetCluster and GetPlacement functions public.
    - Updated references to use the new public functions.
    
    Signed-off-by: Parikshith <pbyregow@redhat.com>

---
+++ e2e/util/placement.go
@@ -0,0 +1,124 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package util
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	k8stypes "k8s.io/apimachinery/pkg/types"
+	"open-cluster-management.io/api/cluster/v1beta1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func GetCurrentCluster(client client.Client, namespace string, placementName string) (string, error) {
+	placementDecision, err := waitPlacementDecision(client, namespace, placementName)
+	if err != nil {
+		return "", err
+	}
+
+	return placementDecision.Status.Decisions[0].ClusterName, nil
+}
+
+func GetCurrentAndTargetCluster(client client.Client, namespace, placementName string) (string, string, error) {
+	drpolicy, err := GetDRPolicy(client, DefaultDRPolicyName)
+	if err != nil {
+		return "", "", err
+	}
+
+	currentCluster, err := GetCurrentCluster(client, namespace, placementName)
+	if err != nil {
+		return "", "", err
+	}
+
+	var targetCluster string
+	if currentCluster == drpolicy.Spec.DRClusters[0] {
+		targetCluster = drpolicy.Spec.DRClusters[1]
+	} else {
+		targetCluster = drpolicy.Spec.DRClusters[0]
+	}
+
+	return currentCluster, targetCluster, nil
+}
+
+func GetPlacement(client client.Client, namespace, name string) (*v1beta1.Placement, error) {
+	placement := &v1beta1.Placement{}
+	key := k8stypes.NamespacedName{Namespace: namespace, Name: name}
+
+	err := client.Get(context.Background(), key, placement)
+	if err != nil {
+		return nil, err
+	}
+
+	return placement, nil
+}
+
+// waitPlacementDecision waits until we have a placement decision and returns the placement decision object.
+func waitPlacementDecision(client client.Client, namespace string, placementName string,
+) (*v1beta1.PlacementDecision, error) {
+	startTime := time.Now()
+
+	for {
+		placement, err := GetPlacement(client, namespace, placementName)
+		if err != nil {
+			return nil, err
+		}
+
+		placementDecision, err := getPlacementDecisionFromPlacement(client, placement)
+		if err != nil {
+			return nil, err
+		}
+
+		if placementDecision != nil && len(placementDecision.Status.Decisions) > 0 {
+			return placementDecision, nil
+		}
+
+		if time.Since(startTime) > Timeout {
+			return nil, fmt.Errorf("timeout waiting for placement decisions for %q ", placementName)
+		}
+
+		time.Sleep(RetryInterval)
+	}
+}
+
+func getPlacementDecisionFromPlacement(ctrlClient client.Client, placement *v1beta1.Placement,
+) (*v1beta1.PlacementDecision, error) {
+	matchLabels := map[string]string{
+		v1beta1.PlacementLabel: placement.GetName(),
+	}
+
+	listOptions := []client.ListOption{
+		client.InNamespace(placement.GetNamespace()),
+		client.MatchingLabels(matchLabels),
+	}
+
+	plDecisions := &v1beta1.PlacementDecisionList{}
+	if err := ctrlClient.List(context.Background(), plDecisions, listOptions...); err != nil {
+		return nil, fmt.Errorf("failed to list PlacementDecisions (placement: %s)",
+			placement.GetNamespace()+"/"+placement.GetName())
+	}
+
+	if len(plDecisions.Items) == 0 {
+		return nil, nil
+	}
+
+	if len(plDecisions.Items) > 1 {
+		return nil, fmt.Errorf("multiple PlacementDecisions found for Placement (count: %d, placement: %s)",
+			len(plDecisions.Items), placement.GetNamespace()+"/"+placement.GetName())
+	}
+
+	plDecision := plDecisions.Items[0]
+	// r.Log.Info("Found ClusterDecision", "ClsDedicision", plDecision.Status.Decisions)
+
+	if len(plDecision.Status.Decisions) > 1 {
+		return nil, fmt.Errorf("multiple placements found in PlacementDecision"+
+			" (count: %d, Placement: %s, PlacementDecision: %s)",
+			len(plDecision.Status.Decisions),
+			placement.GetNamespace()+"/"+placement.GetName(),
+			plDecision.GetName()+"/"+plDecision.GetNamespace())
+	}
+
+	return &plDecision, nil
+}
