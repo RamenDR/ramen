cbd7499210993b46a2b843ffb29ea6f5f84959bb
Nir Soffer <nsoffer@redhat.com>
2025-03-04 21:13:24 +0200

    e2e: Create deployer from name
    Similar to workloads, we need to create a deployer from a string. Add
    similar New() and AvailableNames() functions to make it possible.
    
    Signed-off-by: Nir Soffer <nsoffer@redhat.com>

---
+++ e2e/deployers/deployers.go
@@ -0,0 +1,44 @@
+// SPDX-FileCopyrightText: The RamenDR authors
+// SPDX-License-Identifier: Apache-2.0
+
+package deployers
+
+import (
+	"fmt"
+
+	"github.com/ramendr/ramen/e2e/types"
+)
+
+var registry map[string]types.Deployer
+
+func init() {
+	appset := &ApplicationSet{}
+	subscr := &Subscription{}
+	disapp := &DiscoveredApp{}
+
+	registry = map[string]types.Deployer{
+		appset.GetName(): appset,
+		subscr.GetName(): subscr,
+		disapp.GetName(): disapp,
+	}
+}
+
+// New creates a new deployer for name
+func New(name string) (types.Deployer, error) {
+	deployer := registry[name]
+	if deployer == nil {
+		return nil, fmt.Errorf("unknown deployer %q (choose from %q)", name, AvailableNames())
+	}
+
+	return deployer, nil
+}
+
+func AvailableNames() []string {
+	// TODO: Use maps.Keys() when we have Go 1.23.
+	keys := make([]string, 0, len(registry))
+	for k := range registry {
+		keys = append(keys, k)
+	}
+
+	return keys
+}
