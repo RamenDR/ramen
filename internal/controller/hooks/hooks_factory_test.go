d9243051c9c93a959d3545985b062e9f291f9a68
Annaraya Narasagond <annarayanarasagond@gmail.com>
2025-03-21 12:05:03 +0530

    recipe: Adding test cases for exec hook
    Adding unit test for hooks_factory.
    Adding unit test for exec hooks using fake client.
    
    Signed-off-by: Annaraya Narasagond <annarayanarasagond@gmail.com>

---
+++ internal/controller/hooks/hooks_factory_test.go
@@ -0,0 +1,40 @@
+package hooks_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/ramendr/ramen/internal/controller/hooks"
+	"github.com/ramendr/ramen/internal/controller/kubeobjects"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+)
+
+func TestGetHookExecutor(t *testing.T) {
+	client := fake.NewFakeClient()
+
+	executor, err := hooks.GetHookExecutor(getHookSpecForFactoryTest("check"), client, client.Scheme())
+	assert.Nil(t, err)
+
+	_, ok := executor.(hooks.CheckHook)
+	assert.True(t, ok)
+
+	executor, err = hooks.GetHookExecutor(getHookSpecForFactoryTest("exec"), client, client.Scheme())
+	assert.Nil(t, err)
+
+	_, ok = executor.(hooks.ExecHook)
+	assert.True(t, ok)
+
+	executor, err = hooks.GetHookExecutor(getHookSpecForFactoryTest("undefined"), client, client.Scheme())
+
+	assert.Nil(t, executor)
+	assert.EqualError(t, err, "unsupported hook type")
+}
+
+func getHookSpecForFactoryTest(hookType string) kubeobjects.HookSpec {
+	return kubeobjects.HookSpec{
+		Name:      "test",
+		Namespace: "test-ns",
+		Type:      hookType,
+	}
+}
