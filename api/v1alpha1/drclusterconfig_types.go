4fdd260d4875c5871460028c47906ed4ce02c650
Shyamsundar Ranganathan <srangana@redhat.com>
2025-04-16 09:17:15 -0400

    Add status fields to DRClusterConfig to list available classes
    Currently we create OCM ClusterClaims for each class that we detect on
    a cluster. This CRD change is to change this to reporting the same in
    the status of the DRClusterConfig resource instead.
    
    This is to avoid any limitations on the number of ClusterClaims that may
    be enforced by OCM on a cluster.
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>

--- api/v1alpha1/drclusterconfig_types.go
+++ api/v1alpha1/drclusterconfig_types.go
@@ -13,6 +13,8 @@ import (
 // the same at the hub cluster.
 // It is expected to be watched and used by storage providers that require meta information regarding the cluster
 // and to prepare and manage required storage resources.
+// It also reports detected configuration and health of the cluster, which is used by the OCM hub cluster to process
+// further cross cluster configuration (specifically peerClasses) and report health of the cluster at the hub.
 type DRClusterConfigSpec struct {
 	// ReplicationSchedules desired from storage providers for replicating Persistent Volume data to a peer cluster.
 	// Values are in the form <num><m,h,d>. Where <num> is a number, 'm' indicates minutes, 'h' means hours and
@@ -36,10 +38,18 @@ const (
 
 // DRClusterConfigStatus defines the observed state of DRClusterConfig
 type DRClusterConfigStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
 	Conditions []metav1.Condition `json:"conditions,omitempty"`
+
+	// StorageClasses lists the detected storage classes on the cluster that carry the ramen storageid label
+	StorageClasses []string `json:"storageClasses,omitempty"`
+
+	// VolumeSnapshotClasses lists the detected volume snapshot classes on the cluster that carry the ramen
+	// storageid label
+	VolumeSnapshotClasses []string `json:"volumeSnapshotClasses,omitempty"`
+
+	// VolumeReplicationClasses lists the detected volume replication classes on the cluster that carry the ramen
+	// replicationid label
+	VolumeReplicationClasses []string `json:"volumeReplicationClasses,omitempty"`
 }
 
 //+kubebuilder:object:root=true
