968c5ab2f7051550e23beae84e0de97cf9806071
Shyamsundar Ranganathan <srangana@redhat.com>
2025-04-17 19:27:53 -0400

    Refactor haveOverlappingMetroZones to return a list of cluster lists
    As a part of deprecating spec.Region from DRCluster, the way to process
    overlapping metro DRPolicies was amended to return different maps
    from the corresponsing region based or peerClass based policies.
    
    This is now refactored to return only a list of list of clusters as
    required to detect overlapping metro zones.
    
    Signed-off-by: Shyamsundar Ranganathan <srangana@redhat.com>
    (cherry picked from commit d61f673c795748a501aa19c4bea89b61a4b574b8)

--- internal/controller/drplacementcontrol_controller.go
+++ internal/controller/drplacementcontrol_controller.go
@@ -405,7 +405,7 @@ func (r *DRPlacementControlReconciler) createDRPCInstance(
 
 	d.drType = DRTypeAsync
 
-	isMetro, _ := dRPolicySupportsMetro(drPolicy, drClusters)
+	isMetro, _ := dRPolicySupportsMetro(drPolicy, drClusters, nil)
 	if isMetro {
 		d.volSyncDisabled = true
 		d.drType = DRTypeSync
@@ -1465,7 +1465,7 @@ func (r *DRPlacementControlReconciler) setDRPCMetrics(ctx context.Context,
 	}
 
 	// do not set sync metrics if metro-dr
-	isMetro, _ := dRPolicySupportsMetro(drPolicy, drClusters)
+	isMetro, _ := dRPolicySupportsMetro(drPolicy, drClusters, nil)
 	if isMetro {
 		return nil
 	}
