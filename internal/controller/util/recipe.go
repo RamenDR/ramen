879c456d45d09796fe5e9e45fcb6e7c637c92c65
Annaraya Narasagond <annarayanarasagond@gmail.com>
2025-06-23 13:37:00 +0530

    recipe: skip logging of recipe for pvc updates
    Currently, recipe before and after parameters expand is being logged in
    following scenarios:
    1. When a PVC update is being processed, that is when either a pvc is
       either created or updated.
    2. When vrg is being processed which refers to recipe and its expansion.
    
    The logging of the recipe during pvc update leads to lot of logging.
    Hence, by using the context values, logging is not done now for pvc
    updates.
    
    Signed-off-by: Annaraya Narasagond <annarayanarasagond@gmail.com>

--- internal/controller/util/recipe.go
+++ internal/controller/util/recipe.go
@@ -9,6 +9,8 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
+const RecipeElementsGetForPVC = "recipeElementsGetForPVC"
+
 type RecipeElements struct {
 	PvcSelector         PvcSelector
 	CaptureWorkflow     []kubeobjects.CaptureSpec
