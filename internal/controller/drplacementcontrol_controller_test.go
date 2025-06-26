43860f29043df9f59a47faa09daf9f41ab1d37d7
Abhijeet Shakya <abhijeetshakya21@gmail.com>
2025-06-26 19:40:03 +0530

    Updating missed UT for the ServiceAccount fix
    Signed-off-by: Abhijeet Shakya <abhijeetshakya21@gmail.com>

--- internal/controller/drplacementcontrol_controller_test.go
+++ internal/controller/drplacementcontrol_controller_test.go
@@ -1055,7 +1055,7 @@ func verifyVRGManifestWorkCreatedAsPrimary(namespace, managedCluster string) {
 		return err == nil
 	}, timeout, interval).Should(BeTrue())
 
-	Expect(len(createdVRGRolesManifest.Spec.Workload.Manifests)).To(Equal(12))
+	Expect(len(createdVRGRolesManifest.Spec.Workload.Manifests)).To(Equal(8))
 
 	vrgClusterRoleManifest := createdVRGRolesManifest.Spec.Workload.Manifests[0]
 	Expect(vrgClusterRoleManifest).ToNot(BeNil())
