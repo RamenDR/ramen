85f5f2ca81a51f378d93619ce3eae6cf932a7817
rakeshgm <rakeshgm@redhat.com>
2025-07-03 21:57:46 +0530

    Address review comments
    Signed-off-by: rakeshgm <rakeshgm@redhat.com>

--- internal/controller/util/mcv_util.go
+++ internal/controller/util/mcv_util.go
@@ -11,8 +11,8 @@ import (
 
 	volrep "github.com/csi-addons/kubernetes-csi-addons/api/replication.storage/v1alpha1"
 	"github.com/go-logr/logr"
-	groupsnapv1beta1 "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumegroupsnapshot/v1beta1"
 	snapv1 "github.com/kubernetes-csi/external-snapshotter/client/v8/apis/volumesnapshot/v1"
+	groupsnapv1beta1 "github.com/red-hat-storage/external-snapshotter/client/v8/apis/volumegroupsnapshot/v1beta1"
 	storagev1 "k8s.io/api/storage/v1"
 	k8serrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
