ecc97363f0cd02b4e2fdb634e054d21a0d8c8ae4
Raghavendra Talur <raghavendra.talur@gmail.com>
2025-07-10 14:10:02 -0400

    e2e: make deployers register themselves
    This provides a mechanism for the deployers to provide a factory
    function to create a new instance type of the deployer. We can enhance
    the factory function to take specification for the deployer later on.
    
    Co-Authored-by: Annaraya Narasagond <annarayanarasagond@gmail.com>
    Signed-off-by: Raghavendra Talur <raghavendra.talur@gmail.com>

--- e2e/deployers/deployers.go
+++ e2e/deployers/deployers.go
@@ -5,36 +5,58 @@ package deployers
 
 import (
 	"fmt"
+	"sync"
 
 	"github.com/ramendr/ramen/e2e/types"
 )
 
-var registry map[string]types.Deployer
+// factoryFunc is the new() function type for deployers
+type factoryFunc func() types.Deployer
 
-func init() {
-	appset := &ApplicationSet{}
-	subscr := &Subscription{}
-	disapp := &DiscoveredApp{}
+var mutex sync.Mutex
 
-	registry = map[string]types.Deployer{
-		appset.GetName(): appset,
-		subscr.GetName(): subscr,
-		disapp.GetName(): disapp,
+var registry map[string]factoryFunc
+
+// register needs to be called by every deployer in the init() function to
+// register itself with the deployer registry.
+func register(deployerType string, f factoryFunc) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if registry == nil {
+		registry = make(map[string]factoryFunc)
 	}
+
+	if _, exists := registry[deployerType]; exists {
+		panic(fmt.Sprintf("deployer %q already registered", deployerType))
+	}
+
+	registry[deployerType] = f
+}
+
+func getFactory(name string) factoryFunc {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	return registry[name]
 }
 
 // New creates a new deployer for name
 func New(name string) (types.Deployer, error) {
-	deployer := registry[name]
-	if deployer == nil {
+	factory := getFactory(name)
+	if factory == nil {
 		return nil, fmt.Errorf("unknown deployer %q (choose from %q)", name, AvailableNames())
 	}
 
-	return deployer, nil
+	return factory(), nil
 }
 
 func AvailableNames() []string {
+	mutex.Lock()
+	defer mutex.Unlock()
+
 	keys := make([]string, 0, len(registry))
+
 	for k := range registry {
 		keys = append(keys, k)
 	}
