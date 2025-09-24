6f3cac318a3e92b73451c164c0517b2208c27db0
pruthvitd <prd@redhat.com>
2025-09-24 08:35:27 -0700

    Ensure DRPC transition status remain at WaitForUserToCleanup until secondaries cleanedup
    Signed-off-by: pruthvitd <prd@redhat.com>

--- internal/controller/drplacementcontrol.go
+++ internal/controller/drplacementcontrol.go
@@ -2047,7 +2047,15 @@ func (d *DRPCInstance) EnsureCleanup(clusterToSkip string) error {
 		condition.ObservedGeneration == d.instance.Generation {
 		d.log.Info("Condition values tallied, cleanup is considered complete")
 
-		return nil
+		for cluster, vrg := range d.vrgs {
+			if cluster == clusterToSkip {
+				continue
+			}
+
+			if vrg.Status.State == rmn.SecondaryState {
+				return nil
+			}
+		}
 	}
 
 	d.log.Info(fmt.Sprintf("PeerReady Condition is %s, msg: %s", condition.Status, condition.Message))
