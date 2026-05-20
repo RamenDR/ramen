26f3f20bfa45c8ff5486fb1831bf6a99de841eba
Elena Gershkovich <elenage@il.ibm.com>
2026-05-20 15:03:48 +0300

    Deploy odf-snapshot-controller
    Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>

--- test/drenv/addons/odf_external_snapshotter/start.py
+++ test/drenv/addons/odf_external_snapshotter/start.py
@@ -4,13 +4,38 @@
 from drenv import cache as _cache
 from drenv import kubectl
 
-from .config import CACHE_KEY, PACKAGE_DIR
+from .config import CONTROLLER_CACHE_KEY, CRDS_CACHE_KEY, PACKAGE_DIR
 
 
 def start(cluster):
+    """
+    Deploy external-snapshotter CRDs and controller, and wait until ready.
+    """
+    deploy(cluster)
+    wait(cluster)
+
+
+def deploy(cluster):
     print("Deploying crds")
-    path = _cache.get(str(PACKAGE_DIR / "start-data" / "crds"), CACHE_KEY)
+    path = _cache.get(str(PACKAGE_DIR / "start-data" / "crds"), CRDS_CACHE_KEY)
     kubectl.apply("--filename", path, context=cluster)
 
     print("Waiting until crds are established")
     kubectl.wait("--for=condition=established", "--filename", path, context=cluster)
+
+    print("Deploying snapshot-controller")
+    path = _cache.get(
+        str(PACKAGE_DIR / "start-data" / "controller"), CONTROLLER_CACHE_KEY
+    )
+    kubectl.apply("--filename", path, context=cluster)
+
+
+def wait(cluster):
+    print("Waiting until snapshot-controller is rolled out")
+    kubectl.rollout(
+        "status",
+        "deploy/odf-external-snapshotter-operator",
+        "--namespace=kube-system",
+        "--timeout=300s",
+        context=cluster,
+    )
