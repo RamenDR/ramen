RamenDR/ramen

commit 831a6c47d12bd810e331f5b13136ff7ba8007806

Implement PV annotation with destination volume info for volume groups
Signed-off-by: Elena Gershkovich <elenage@il.ibm.com>

internal/controller/vrg_volgrouprep.go
@@ -55,6 +55,18 @@ func (v *VRGInstance) reconcileVolGroupRepsAsPrimary(groupPVCs map[types.Namespa
 		for idx := range pvcs {
 			pvc := pvcs[idx]
 
+			// Annotate the PV with the destination volume handle if available from VR status.
+			// This allows the secondary cluster to restore the PV with the correct volume handle
+			// when source and destination volume IDs differ.
+			if err := v.annotateWithDestinationVolumeHandleForVolGroupRep(vgrNamespacedName, pvc); err != nil {
+				log.Error(err, fmt.Sprintf("failed to annotate PV of PVC %s with destination volume handle",
+					pvc.Name))
+
+				v.requeue()
+
+				continue
+			}
+
 			if err := v.uploadPVandPVCtoS3Stores(pvc, log); err != nil {
 				log.Error(err, "Requeuing due to failure to upload PV/PVC object to S3 store(s)")
 
@@ -928,6 +940,57 @@ func (v *VRGInstance) deleteVGR(vrNamespacedName types.NamespacedName, log logr.
 	v.log.Info("Deleted VolumeGroupReplication resource %s/%s", vrNamespacedName.Namespace, vrNamespacedName.Name)
 
 	return v.ensureVRDeletedFromAPIServer(vrNamespacedName, cr, log)
+}
+
+// annotateWithDestinationVolumeHandleForVolGroupRep looks up the VolumeGroupReplication for the PVC
+// and annotates the PV with the destination volume handle if available.
+func (v *VRGInstance) annotateWithDestinationVolumeHandleForVolGroupRep(vrNamespacedName types.NamespacedName,
+	pvc *corev1.PersistentVolumeClaim,
+) error {
+	pv, err := v.getPVFromPVC(pvc)
+	if err != nil {
+		return fmt.Errorf("failed to get PV for PVC %s: %w", pvc.Name, err)
+	}
+
+	vgr := &volrep.VolumeGroupReplication{}
+
+	if err := v.reconciler.Get(v.ctx, vrNamespacedName, vgr); err != nil {
+		v.log.Info(fmt.Sprintf("failed to get VGR %s for PV %s err %s", vrNamespacedName.Name, pv.Name, err))
+
+		return err
+	}
+
+	available, err := v.destinationInfoAvailableOrSkip(vgr.Status.Conditions,
+		fmt.Sprintf("VGR %s", vgr.Name), pv.Name)
+	if err != nil {
+		return err
+	}
+
+	if !available {
+		return nil
+	}
+
+	vgrc, err := v.getVGRCFromVGR(vgr)
+	if err != nil {
+		v.log.Error(err, "failed to annotate PV %s for PVC - no VGRContent found", "pvc", pvc.Name)
+
+		return fmt.Errorf("failed to get VolumeGroupReplicationContent for VGR %s: %w", vgr.Name, err)
+	}
+
+	for _, pvMapping := range vgrc.Status.PersistentVolumeMappingList {
+		if pvMapping.Name != pv.Name {
+			continue
+		}
+
+		if pvMapping.DestinationVolumeHandle == "" {
+			return fmt.Errorf("destination volume ID is empty for VGR %s", vgr.Name)
+		}
+
+		return v.applyDestinationVolumeHandleToPV(&pv, pvMapping.DestinationVolumeHandle)
+	}
+
+	return fmt.Errorf("no persistent volume mapping for PV %s in VolumeGroupReplicationContent %s",
+		pv.Name, vgrc.Name)
 }
 
 func (v *VRGInstance) addArchivedAnnotationForVGRandVGRC(vgr *volrep.VolumeGroupReplication, log logr.Logger) error {

go.sum
@@ -158,8 +158,8 @@ github.com/google/gofuzz v1.1.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/
 github.com/google/gofuzz v1.2.0 h1:xRy4A+RhZaiKjJ1bPfwQ8sedCA+YS2YcCHW6ec7JMi0=
 github.com/google/gofuzz v1.2.0/go.mod h1:dBl0BpW6vV/+mYPU4Po3pmUjxk6FQPldtuIdl/M65Eg=
 github.com/google/pprof v0.0.0-20210407192527-94a9f03dee38/go.mod h1:kpwsk12EmLew5upagYY7GY0pfYCcupk39gWOCRROcvE=
-github.com/google/pprof v0.0.0-20250403155104-27863c87afa6 h1:BHT72Gu3keYf3ZEu2J0b1vyeLSOYI8bm5wbJM/8yDe8=
-github.com/google/pprof v0.0.0-20250403155104-27863c87afa6/go.mod h1:boTsfXsheKC2y+lKOCMpSfarhxDeIzfZG1jqGcPl3cA=
+github.com/google/pprof v0.0.0-20260115054156-294ebfa9ad83 h1:z2ogiKUYzX5Is6zr/vP9vJGqPwcdqsWjOt+V8J7+bTc=
+github.com/google/pprof v0.0.0-20260115054156-294ebfa9ad83/go.mod h1:MxpfABSjhmINe3F1It9d+8exIHFvUqtLIRCdOGNXqiI=
 github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510 h1:El6M4kTTCOh6aBiKaUGG7oYTSPP8MxqL4YI3kZKwcP4=
 github.com/google/shlex v0.0.0-20191202100458-e7afc7fbc510/go.mod h1:pupxD2MaaD3pAXIBCelhxNneeOaAeabZDe5s4K6zSpQ=
 github.com/google/uuid v1.1.2/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=

internal/controller/vrg_volrep_test.go
@@ -3048,10 +3048,80 @@ func (v *vrgTest) waitForVGRCountToMatch(vgrCount int) {
 		Expect(err).NotTo(HaveOccurred(),
 			"failed to get a list of VGRs in namespace %s", v.namespace)
 
+		for i := range volGroupRepList.Items {
+			v.ensureVolumeGroupReplicationContentForTesting(types.NamespacedName{
+				Namespace: volGroupRepList.Items[i].Namespace,
+				Name:      volGroupRepList.Items[i].Name,
+			})
+		}
+
 		return len(volGroupRepList.Items)
 	}, timeout, interval).Should(BeNumerically("==", vgrCount),
 		"while waiting for VGR count of %d in VRG %s of namespace %s",
 		vgrCount, v.vrgName, v.namespace)
+}
+
+// ensureVolumeGroupReplicationContentForTesting creates a cluster VolumeGroupReplicationContent and wires
+// VolumeGroupReplication.Spec.VolumeGroupReplicationContentName. Envtest CRDs require spec.volumeGroupReplicationRef
+// and spec.volumeGroupReplicationHandle (see hack/test/replication.storage.openshift.io_volumegroupreplicationcontents.yaml).
+func (v *vrgTest) ensureVolumeGroupReplicationContentForTesting(vgrKey types.NamespacedName) {
+	provisioner := "manual.storage.com"
+	if v.template != nil && v.template.vrcProvisioner != "" {
+		provisioner = v.template.vrcProvisioner
+	}
+
+	err := retry.RetryOnConflict(retry.DefaultBackoff, func() error {
+		vgr := &volrep.VolumeGroupReplication{}
+		if err := k8sClient.Get(context.TODO(), vgrKey, vgr); err != nil {
+			return err
+		}
+
+		contentName := vgr.Spec.VolumeGroupReplicationContentName
+		if contentName == "" {
+			contentName = fmt.Sprintf("%s-%s-vg-content", vgrKey.Namespace, vgrKey.Name)
+		}
+
+		vgrcKey := types.NamespacedName{Name: contentName}
+		existing := &volrep.VolumeGroupReplicationContent{}
+
+		err := k8sClient.Get(context.TODO(), vgrcKey, existing)
+		if k8serrors.IsNotFound(err) {
+			vgrcObj := &volrep.VolumeGroupReplicationContent{
+				ObjectMeta: metav1.ObjectMeta{Name: contentName},
+				Spec: volrep.VolumeGroupReplicationContentSpec{
+					Provisioner:                     provisioner,
+					VolumeGroupReplicationClassName: vgr.Spec.VolumeGroupReplicationClassName,
+					VolumeGroupReplicationHandle:    fmt.Sprintf("ramen-envtest-%s", contentName),
+					VolumeGroupReplicationRef: &corev1.ObjectReference{
+						Kind:       "VolumeGroupReplication",
+						Namespace:  vgr.Namespace,
+						Name:       vgr.Name,
+						UID:        vgr.UID,
+						APIVersion: "replication.storage.openshift.io/v1alpha1",
+					},
+					Source: volrep.VolumeGroupReplicationContentSource{
+						VolumeHandles: []string{"ramen-envtest-vg-handle"},
+					},
+				},
+			}
+
+			createErr := k8sClient.Create(context.TODO(), vgrcObj)
+			if createErr != nil && !k8serrors.IsAlreadyExists(createErr) {
+				return createErr
+			}
+		} else if err != nil {
+			return err
+		}
+
+		if vgr.Spec.VolumeGroupReplicationContentName != contentName {
+			vgr.Spec.VolumeGroupReplicationContentName = contentName
+
+			return k8sClient.Update(context.TODO(), vgr)
+		}
+
+		return nil
+	})
+	Expect(err).NotTo(HaveOccurred(), "ensure VGRC for VGR %s", vgrKey.String())
 }
 
 func (v *vrgTest) promoteVolReps() {
@@ -3220,7 +3290,15 @@ func (v *vrgTest) promoteVolGroupRepsAndDo(options promoteOptions, do func(int, 
 	Expect(err).NotTo(HaveOccurred(), "failed to get a list of VRs in namespace %s", v.namespace)
 
 	for index := range volGroupRepList.Items {
-		volGroup := volGroupRepList.Items[index]
+		volGroupKey := types.NamespacedName{
+			Namespace: volGroupRepList.Items[index].Namespace,
+			Name:      volGroupRepList.Items[index].Name,
+		}
+		v.ensureVolumeGroupReplicationContentForTesting(volGroupKey)
+
+		volGroup := volrep.VolumeGroupReplication{}
+		err = k8sClient.Get(context.TODO(), volGroupKey, &volGroup)
+		Expect(err).NotTo(HaveOccurred(), "failed to re-get VolGroupRep %s", volGroupKey.String())
 
 		volGroupRepStatus := volrep.VolumeGroupReplicationStatus{
 			VolumeReplicationStatus: volrep.VolumeReplicationStatus{

commit 5cb6bb36f22d9aae354a76413ca385b6f87a9b30

Fix MetroDR race condition: Handle VR/VGR not found gracefully
This fixes a critical race condition introduced in PR #2547 that breaks
MetroDR scenarios. When VolumeReplication or VolumeGroupReplication
resources are newly created, they may not be immediately available in
the API server cache. The annotation logic was failing with 'not found'
errors, blocking PV protection and S3 uploads.

Changes:
- annotateWithDestinationVolumeHandleForVolRep(): Skip gracefully when
  VR is not yet available, allowing retry on next reconcile
- annotateWithDestinationVolumeHandleForVolGroupRep(): Same fix for VGR

This is particularly critical for MetroDR (synchronous replication) where
VRs are created more frequently during initial setup.

Fixes: VolumeReplication not found error in MetroDR scenarios
Related: PR #2547 (destination volume handle support)

internal/controller/vrg_volgrouprep.go
@@ -955,6 +955,13 @@ func (v *VRGInstance) annotateWithDestinationVolumeHandleForVolGroupRep(vrNamesp
 	vgr := &volrep.VolumeGroupReplication{}
 
 	if err := v.reconciler.Get(v.ctx, vrNamespacedName, vgr); err != nil {
+		if k8serrors.IsNotFound(err) {
+			v.log.Info(fmt.Sprintf("VGR %s not yet available for PV %s, will retry on next reconcile",
+				vrNamespacedName.Name, pv.Name))
+
+			return nil
+		}
+
 		v.log.Info(fmt.Sprintf("failed to get VGR %s for PV %s err %s", vrNamespacedName.Name, pv.Name, err))
 
 		return err

internal/controller/vrg_volrep.go
@@ -843,6 +843,13 @@ func (v *VRGInstance) annotateWithDestinationVolumeHandleForVolRep(pvc *corev1.P
 	vrNamespacedName := types.NamespacedName{Name: pvc.Name, Namespace: pvc.Namespace}
 
 	if err := v.reconciler.Get(v.ctx, vrNamespacedName, volRep); err != nil {
+		if k8serrors.IsNotFound(err) {
+			v.log.Info(fmt.Sprintf("VR %s not yet available for PV %s, will retry on next reconcile",
+				pvc.Name, pv.Name))
+
+			return nil
+		}
+
 		v.log.Info(fmt.Sprintf("failed to get VR %s for PV %s err %s", pvc.Name, pv.Name, err))
 
 		return err