ea7430253a01472d0a4662c39b506059e059af88
raaizik <132667934+raaizik@users.noreply.github.com>
2026-08-18 13:45:35 +0300

    s3: include OrigErr in AWS error wrapping
    RequestError logs only showed "send request failed", dropping the
    underlying x509/timeout/reset. Keep code and message, and append
    OrigErr when present so operator logs are diagnosable.
    
    Fixes #2724
    
    Signed-off-by: raaizik <132667934+raaizik@users.noreply.github.com>

--- internal/controller/s3utils.go
+++ internal/controller/s3utils.go
@@ -424,12 +424,22 @@ func DeleteTypedObject(s ObjectStorer, keyPrefix, keySuffix string, object inter
 }
 
 func processAwsError(errMsgPrefix, err error) error {
-	var awsErr awserr.Error
-	if errors.As(err, &awsErr) {
-		return fmt.Errorf("%w: code: %s, message: %s", errMsgPrefix, awsErr.Code(), awsErr.Message())
+	if err == nil {
+		return errMsgPrefix
 	}
 
-	return errMsgPrefix
+	var awsErr awserr.Error
+	if !errors.As(err, &awsErr) {
+		return fmt.Errorf("%w: %w", errMsgPrefix, err)
+	}
+
+	if origErr := awsErr.OrigErr(); origErr != nil {
+		return fmt.Errorf("%w: code: %s, message: %s, orig: %v",
+			errMsgPrefix, awsErr.Code(), awsErr.Message(), origErr)
+	}
+
+	return fmt.Errorf("%w: code: %s, message: %s",
+		errMsgPrefix, awsErr.Code(), awsErr.Message())
 }
 
 // UploadObject uploads the given object to the bucket with the given key.
